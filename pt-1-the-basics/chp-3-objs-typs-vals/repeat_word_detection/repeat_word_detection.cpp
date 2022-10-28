e94a3a9739c0794454470244d431b72cf300e47f
Jason Armstrong <jasnarmstrng@gmail.com>
2022-10-28 20:56:26 +0100

    Update program to indicate the position in stream of the repeated word.

--- pt-1-the-basics/chp-3-objs-typs-vals/repeat_word_detection/repeat_word_detection.cpp
+++ pt-1-the-basics/chp-3-objs-typs-vals/repeat_word_detection/repeat_word_detection.cpp
@@ -3,14 +3,17 @@
 #include <iostream>
 int main()
 {
-  std::cout << "Note: To end this program press CTRL+D.\n";
-  std::cout << "Please enter a sentence >= 2 words: ";
+  int word_counter = 0;
   std::string previous = " "; // previous word initialized to not a word. Recall cin skips spaces
   std::string current;
+
+  std::cout << "Note: To end this program press CTRL+D.\n";
+  std::cout << "Please enter a sentence >= 2 words: ";
   while (std::cin >> current) // read a stream of words
   {
+    ++word_counter;
     if (previous == current) // check if current being read is same as last
-      std::cout << "repeated word: " << current << "\n";
+      std::cout << current << " - repeated at position " << word_counter << ".\n";
     previous = current; // current becomes previous before next in stream is read
   }
 }
