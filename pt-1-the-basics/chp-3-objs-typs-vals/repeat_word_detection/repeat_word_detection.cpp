5fda35488ef60b69d3f35e88a5b89a40cb4d41ad
Jason Armstrong <jasnarmstrng@gmail.com>
2022-10-28 20:28:35 +0100

    Update comments.

--- pt-1-the-basics/chp-3-objs-typs-vals/repeat_word_detection/repeat_word_detection.cpp
+++ pt-1-the-basics/chp-3-objs-typs-vals/repeat_word_detection/repeat_word_detection.cpp
@@ -5,12 +5,12 @@ int main()
 {
   std::cout << "Note: To end this program press CTRL+D.\n";
   std::cout << "Please enter a sentence >= 2 words: ";
-  std::string previous = " ";
+  std::string previous = " "; // previous word initialized to not a word. Recall cin skips spaces
   std::string current;
-  while (std::cin >> current)
+  while (std::cin >> current) // read a stream of words
   {
-    if (previous == current)
+    if (previous == current) // check if current being read is same as last
       std::cout << "repeated word: " << current << "\n";
-    previous = current;
+    previous = current; // current becomes previous before next in stream is read
   }
 }
