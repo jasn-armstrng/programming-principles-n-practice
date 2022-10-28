9d16f7c82dcc1fd0c3f165092de56f5cf2345f29
Jason Armstrong <jasnarmstrng@gmail.com>
2022-10-28 13:18:35 +0100

    Update comments.

--- pt-1-the-basics/chp-3-objs-typs-vals/name_and_age/name_and_age.cpp
+++ pt-1-the-basics/chp-3-objs-typs-vals/name_and_age/name_and_age.cpp
@@ -1,11 +1,11 @@
-// This program prompts user to enter their first name and age. Both values will
-// then be logged to the screen.
+// This program prompts user for their first name and age. Both values are
+// then logged to the screen.
 
 #include <iostream>
 int main(){
-  std::cout << "Please enter your first name and age\n";
-  std::string first_name = "???"; // String variable with default value
+  std::cout << "Please enter your first name and age\n"; // prompt for user info.
+  std::string first_name = "???"; // string variable with default value "???" meaning "don't know the name"
   double age = -1.0; // double variable (default -1 means "don't know the age")
-  std::cin >> first_name >> age;
+  std::cin >> first_name >> age; // read a string followed by an int/double
   std::cout << "Hello, " << first_name << " (age " << age << ")\n";
 }
