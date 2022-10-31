127629694258af922cd7a528327d050a15d53906
Jason Armstrong <jasnarmstrng@gmail.com>
2022-10-31 15:06:27 +0000

    Add prompt for users values.

--- pt-1-the-basics/chp-3-objs-typs-vals/exercises/operations_on_integers/operations_on_integers.cpp
+++ pt-1-the-basics/chp-3-objs-typs-vals/exercises/operations_on_integers/operations_on_integers.cpp
@@ -5,3 +5,24 @@
 // 4. diff(val1, val2)
 // 5. mult(val1, val2)
 // 6. div(val1, val2)
+
+#include <iostream>
+
+std::string min_max(int v1, int v2)
+{
+  if (v1 > v2)
+    return std::to_string(v1) + " is larger than " + std::to_string(v2);
+  else if (v1 < v2)
+    return std::to_string(v2) + " is larger than " + std::to_string(v1);
+  else
+    return "Both are equal.";
+}
+
+int main()
+{
+  int val1, val2;
+  std::cout << "Please enter 2 integer values: ";
+  std::cin >> val1 >> val2;
+  std::cout << val1 << val2;
+  return 0;
+}
