27c9ffd201945ee59ec56e1bdda3432af8dcbbb3
Jason Armstrong <jasnarmstrng@gmail.com>
2022-10-31 17:49:37 +0000

    Build and test.

--- pt-1-the-basics/chp-3-objs-typs-vals/exercises/integer_sort/integer_sort.cpp
+++ pt-1-the-basics/chp-3-objs-typs-vals/exercises/integer_sort/integer_sort.cpp
@@ -3,9 +3,28 @@
 #include <iostream>
 int main()
 {
-  int val2, val2, val3;
+  int val1, val2, val3;
+  double first, second, third;
   std::cout << "+--------------+\n";
   std::cout << "| Integer Sort |\n";
   std::cout << "+--------------+\n\n";
   std::cout << "Please enter 3 integer values: ";
+  std::cin >> val1 >> val2 >> val3;
+
+  if (val1 > val2)
+    first = val2;
+    second = val1;
+
+  // if (first >= val3)
+  //   third = first;
+  //   first = val3;
+
+  // if (second >= third)
+  //   second ^= third;
+  //   third ^= second;
+  //   second ^= third;
+
+  std::cout << val1 << ' ' << val2 << ' ' << val3 << '\n';
+  std::cout << first << ' ' << second << '\n';
+  return 0;
 }
