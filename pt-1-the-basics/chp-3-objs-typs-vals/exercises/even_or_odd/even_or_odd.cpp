9118acfe974c862721c42c868202c212d2b976fd
Jason Armstrong <jasnarmstrng@gmail.com>
2022-10-31 19:27:21 +0000

    Update even/odd check.

--- pt-1-the-basics/chp-3-objs-typs-vals/exercises/even_or_odd/even_or_odd.cpp
+++ pt-1-the-basics/chp-3-objs-typs-vals/exercises/even_or_odd/even_or_odd.cpp
@@ -10,10 +10,10 @@ int main()
   std::cout << "Please enter an integer: ";
   std::cin >> num;
 
-  if (num & 1 == 0)
+  if (num % 2 == 0)
     std::cout << "\nThe value " << num << " is an even number.\n";
   else
-    std::cout << "\nThe value " << num << " is an even number.\n";
+    std::cout << "\nThe value " << num << " is an odd number.\n";
 
   return 0;
 }
