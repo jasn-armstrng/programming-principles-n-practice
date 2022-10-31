fa5167768ea84b7f7d34d6c9b0071887c85b89b0
Jason Armstrong <jasnarmstrng@gmail.com>
2022-10-31 19:19:16 +0000

    Add prompt for user value. Add even/odd check.

---
+++ pt-1-the-basics/chp-3-objs-typs-vals/exercises/even_or_odd/even_or_odd.cpp
@@ -0,0 +1,19 @@
+// This program checks if user integer input is even or odd.
+
+#include <iostream>
+int main()
+{
+  int num;
+  std::cout << "+-------------+\n";
+  std::cout << "| Even or Odd |\n";
+  std::cout << "+-------------+\n\n";
+  std::cout << "Please enter an integer: ";
+  std::cin >> num;
+
+  if (num & 1 == 0)
+    std::cout << "\nThe value " << num << " is an even number.\n";
+  else
+    std::cout << "\nThe value " << num << " is an even number.\n";
+
+  return 0;
+}
