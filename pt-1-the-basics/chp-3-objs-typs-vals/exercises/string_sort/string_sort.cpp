ce7062e115d06f9f23cc407d78a617d6ad3de4a3
Jason Armstrong <jasnarmstrng@gmail.com>
2022-10-31 19:02:44 +0000

    Add conditionals for string comparison.

--- pt-1-the-basics/chp-3-objs-typs-vals/exercises/string_sort/string_sort.cpp
+++ pt-1-the-basics/chp-3-objs-typs-vals/exercises/string_sort/string_sort.cpp
@@ -4,18 +4,34 @@
 #include <iostream>
 int main()
 {
-  std::string s1, s2, s3;
+  std::string s1, s2, s3, proxy;
   std::cout << "+-------------+\n";
   std::cout << "| String Sort |\n";
   std::cout << "+-------------+\n\n";
   std::cout << "Please enter 3 string values: ";
   std::cin >> s1 >> s2 >> s3;
 
-  // The following ifs do comparisons and swap using bitwise xor.
-  // if (s1 > s2)
-  // {
+  // The following ifs do comparisons and value swap using a proxy.
+  if (s1 > s2)
+  {
+    proxy = s1;
+    s1 = s2;
+    s2 = proxy;
+  }
 
-  // }
+  if (s1 > s3)
+  {
+    proxy = s1;
+    s1 = s3;
+    s3 = proxy;
+  }
+
+  if (s2 > s3)
+  {
+    proxy = s2;
+    s2 = s3;
+    s3 = proxy;
+  }
 
   std::cout << "\nSorted: " << s1 << ' ' << s2 << ' ' << s3 << '\n';
   return 0;
