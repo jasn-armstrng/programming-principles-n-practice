aa36aa18b1ae9de296da48a883032c3bdb3ce5bd
Jason Armstrong <jasnarmstrng@gmail.com>
2022-10-31 14:28:41 +0000

    Build and test miles_to_km.

--- pt-1-the-basics/chp-3-objs-typs-vals/exercises/miles_to_km/miles_to_km.cpp
+++ pt-1-the-basics/chp-3-objs-typs-vals/exercises/miles_to_km/miles_to_km.cpp
@@ -4,7 +4,11 @@
 int main()
 {
   double miles;
-  std::cout << "Please enter a number of miles to convert to km: ";
+  std::cout << "+------------------------------------------+\n";
+  std::cout << "|Welcome to the Miles->Kilometres converter|\n";
+  std::cout << "+------------------------------------------+\n\n";
+  std::cout << "Please enter a number of miles: ";
   std::cin >> miles;
-  std::cout << miles * 1.609;
+  std::cout << "Answer: " << miles * 1.609 << "km\n";
+  return 0;
 }
