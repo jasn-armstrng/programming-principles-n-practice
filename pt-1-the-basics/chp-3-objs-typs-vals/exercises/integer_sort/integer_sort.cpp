3f95ac070473165846bcbd8c1cf2618d30a8ef10
Jason Armstrong <jasnarmstrng@gmail.com>
2022-10-31 15:51:29 +0000

    Add prompt for user values.

---
+++ pt-1-the-basics/chp-3-objs-typs-vals/exercises/integer_sort/integer_sort.cpp
@@ -0,0 +1,11 @@
+// This program sorts 3 integers from user and returns them sorted
+
+#include <iostream>
+int main()
+{
+  int val2, val2, val3;
+  std::cout << "+--------------+\n";
+  std::cout << "| Integer Sort |\n";
+  std::cout << "+--------------+\n\n";
+  std::cout << "Please enter 3 integer values: ";
+}
