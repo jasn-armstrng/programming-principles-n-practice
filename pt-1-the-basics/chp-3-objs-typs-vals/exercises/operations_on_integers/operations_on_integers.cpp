f42b2769cd94fbc0c968b261fd16a380e3f9e5ce
Jason Armstrong <jasnarmstrng@gmail.com>
2022-10-31 14:54:06 +0000

    Create file operations_on_integers.cpp.

---
+++ pt-1-the-basics/chp-3-objs-typs-vals/exercises/operations_on_integers/operations_on_integers.cpp
@@ -0,0 +1,7 @@
+// This program takes as input 2 integers from user and returns these evals:
+// 1. min(val1, val2)
+// 2. max(val1, val2)
+// 3. sum(val1, val2)
+// 4. diff(val1, val2)
+// 5. mult(val1, val2)
+// 6. div(val1, val2)
