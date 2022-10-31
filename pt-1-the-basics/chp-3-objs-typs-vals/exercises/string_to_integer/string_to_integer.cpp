bcfb8512e2bca3f32353dad62bc511dc8b14f54d
Jason Armstrong <jasnarmstrng@gmail.com>
2022-10-31 20:12:36 +0000

    Build and test.

--- pt-1-the-basics/chp-3-objs-typs-vals/exercises/string_to_integer/string_to_integer.cpp
+++ pt-1-the-basics/chp-3-objs-typs-vals/exercises/string_to_integer/string_to_integer.cpp
@@ -4,10 +4,27 @@
 #include <iostream>
 int main()
 {
-  std::string val;
+  std::string in;
+  int out = -1;
   std::cout << "+-------------------+\n";
   std::cout << "| String to Integer |\n";
   std::cout << "+-------------------+\n\n";
   std::cout << "Please enter a number as text or integer: ";
-  std::cin >> val;
+  std::cin >> in;
+
+  if (in == "zero" || in == "0")
+    out = 0;
+  else if (in == "one" || in == "1")
+    out = 1;
+  else if (in == "two" || in == "2")
+    out = 2;
+  else if (in == "three" || in == "3")
+    out = 3;
+  else if (in == "four" || in == "4")
+    out = 4;
+
+  if (out != -1)
+    std::cout << "\nResult: The input " << in << " corresponds to the number " << out << ".\n\n";
+  else
+    std::cout << "\nResult: Not a number I know.\n\n";
 }
