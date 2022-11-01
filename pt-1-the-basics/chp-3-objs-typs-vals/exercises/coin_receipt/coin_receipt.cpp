6fec4816073007458af46074caa53c8a06104dff
Jason Armstrong <jasnarmstrng@gmail.com>
2022-11-01 15:13:51 +0000

    Create file coin_receipt.cpp.

---
+++ pt-1-the-basics/chp-3-objs-typs-vals/exercises/coin_receipt/coin_receipt.cpp
@@ -0,0 +1,10 @@
+// This program provides the user a list and value of their coins.
+
+#include <iostream>
+int main()
+{
+  std::string pennies, nickels, dimes, quarters, half_dollars, silver_dollars;
+  std::cout << "+--------------+\n";
+  std::cout << "| Coin Receipt |\n";
+  std::cout << "+--------------+\n\n";
+}
