18a57ba3e9ccadc858792c91ebe4b331ef189ae5
Jason Armstrong <jasnarmstrng@gmail.com>
2022-11-01 15:41:47 +0000

    Add output to user -> list of coins and total.

--- pt-1-the-basics/chp-3-objs-typs-vals/exercises/coin_receipt/coin_receipt.cpp
+++ pt-1-the-basics/chp-3-objs-typs-vals/exercises/coin_receipt/coin_receipt.cpp
@@ -3,16 +3,28 @@
 #include <iostream>
 int main()
 {
-  std::string pennies, nickels, dimes, quarters, half_dollars, silver_dollars;
+  // Variable declaration
+  int pennies, nickels, dimes, quarters, half_dollars, silver_dollars, total_of_coins;
+  // Heading
   std::cout << "+-----------------------------------------+\n";
   std::cout << "| Coin Receipt: List and total your coins |\n";
   std::cout << "+-----------------------------------------+\n\n";
+  // Prompt
   std::cout << "How many pennies do you have?: "; std::cin >> pennies;
-  std::cout << "                     nickels?: "; std::cin >> nickels;
-  std::cout << "                       dimes?: "; std::cin >> dimes;
-  std::cout << "                    quarters?: "; std::cin >> quarters;
-  std::cout << "                half-dollars?: "; std::cin >> half_dollars;
-  std::cout << "              silver-dollars?: "; std::cin >> silver_dollars;
-
+  std::cout << "How many nickels do you have?: "; std::cin >> nickels;
+  std::cout << "How many dimes do you have?: "; std::cin >> dimes;
+  std::cout << "How many quarters do you have?: "; std::cin >> quarters;
+  std::cout << "How many half dollars do you have?: "; std::cin >> half_dollars;
+  std::cout << "How many silver dollars do you have?: "; std::cin >> silver_dollars;
+  total_of_coins = pennies + nickels + dimes + quarters + half_dollars + silver_dollars;
+  // Output
+  std::cout << "\nYour receipt:\n";
+  std::cout << "You have " << pennies << " pennies.\n";
+  std::cout << "You have " << nickels << " nickels.\n";
+  std::cout << "You have " << dimes << " dimes.\n";
+  std::cout << "You have " << quarters << " quarters.\n";
+  std::cout << "You have " << half_dollars << " half dollars.\n";
+  std::cout << "You have " << silver_dollars << " silver dollars.\n";
+  std::cout << "The value of all your coins is " << total_of_coins << " cents.\n";
   return 0;
 }
