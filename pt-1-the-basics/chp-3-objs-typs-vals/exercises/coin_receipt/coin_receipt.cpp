4fa81aa0f678287801dfce231e67f06c845d5ede
Jason Armstrong <jasnarmstrng@gmail.com>
2022-11-01 15:21:05 +0000

    Add prompt for user coin values.

--- pt-1-the-basics/chp-3-objs-typs-vals/exercises/coin_receipt/coin_receipt.cpp
+++ pt-1-the-basics/chp-3-objs-typs-vals/exercises/coin_receipt/coin_receipt.cpp
@@ -4,7 +4,15 @@
 int main()
 {
   std::string pennies, nickels, dimes, quarters, half_dollars, silver_dollars;
-  std::cout << "+--------------+\n";
-  std::cout << "| Coin Receipt |\n";
-  std::cout << "+--------------+\n\n";
+  std::cout << "+-----------------------------------------+\n";
+  std::cout << "| Coin Receipt: List and total your coins |\n";
+  std::cout << "+-----------------------------------------+\n\n";
+  std::cout << "How many pennies do you have?: "; std::cin >> pennies;
+  std::cout << "                     nickels?: "; std::cin >> nickels;
+  std::cout << "                       dimes?: "; std::cin >> dimes;
+  std::cout << "                    quarters?: "; std::cin >> quarters;
+  std::cout << "                half-dollars?: "; std::cin >> half_dollars;
+  std::cout << "              silver-dollars?: "; std::cin >> silver_dollars;
+
+  return 0;
 }
