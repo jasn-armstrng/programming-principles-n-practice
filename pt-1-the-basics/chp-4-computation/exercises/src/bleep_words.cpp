a9f396bc8adbc7a647aafa7094c77354f9890978
Jason Armstrong <jasnarmstrng@gmail.com>
2022-11-03 19:44:00 +0000

    refactor: Move blacklist word check to function

--- pt-1-the-basics/chp-4-computation/exercises/src/bleep_words.cpp
+++ pt-1-the-basics/chp-4-computation/exercises/src/bleep_words.cpp
@@ -3,26 +3,35 @@
 
 #include <iostream>
 #include <vector>
-int main()
+
+std::string check_blacklist(std::string x)
 {
   std::vector<std::string> blacklist = {"fox", "lazy", "dog"};
+  for(std::string w:blacklist)
+    {
+      if (x == w)
+        return "BLEEP";
+    }
+  return x;
+}
+
+int main()
+{
   std::string censored = "";
   std::string current;
-  std::cout << "+----------------+\n";
-  std::cout << "| Bleep my words |\n";
-  std::cout << "+----------------+\n\n";
+  std::cout << "+-------------+\n";
+  std::cout << "| Bleep words |\n";
+  std::cout << "+-------------+\n\n";
 
+  // Prompt for use input
   std::cout << "Enter your text to censor at > below.\n";
   std::cout << "When you are done press Enter then ctrl+D for results.\n";
   std::cout << "\n> ";
 
+  // Read in input and check against blacklist
   while (std::cin >> current)
   {
-    for(std::string w:blacklist)
-    {
-      if (current == w)
-        current = "BLEEP";
-    }
+    current = check_blacklist(current);
     censored += (current + ' ');
   }
 
