900bd6b02d941b7fb69e1348e0e7c44b12bca2f4
Jason Armstrong <jasnarmstrng@gmail.com>
2022-11-03 13:27:25 +0000

    feat: Add conversion calculation based on selection

--- pt-1-the-basics/chp-4-computation/exercises/src/currency_converter.cpp
+++ pt-1-the-basics/chp-4-computation/exercises/src/currency_converter.cpp
@@ -13,18 +13,33 @@ int main()
 {
   double amount;
   int currency;
-  std::cout << "+--------------------+\n";
-  std::cout << "| Currency Converter |\n";
-  std::cout << "+--------------------+\n\n";
+  std::cout << "\t      +--------------------+\n";
+  std::cout << "\t      | Currency Converter |\n";
+  std::cout << "\t      +--------------------+\n\n";
   // Prompt for user values. Read in user input.
   std::cout << "Please enter currency from the numbered options below\n\n";
-  std::cout << "\t1: €\t2: £\t3: ¥\t4: ₩\t5: kr\n\n";
-  std::cout << "Currency: ";
+  std::cout << "\t1: €\t2: £\t3: ¥\t4: ₩\t5: dkk\n\n";
+  std::cout << "Option: ";
   std::cin >> currency;
-  std::cout << "  Amount: ";
+  std::cout << "Amount: ";
   std::cin >> amount;
 
   // Cases for conversion
-  return 0;
+  switch (currency)
+  {
+    case(1):
+      std::cout << "\nResult: " << "€" << amount << " = $" << amount * 0.98 << '\n'; break;
+    case(2):
+      std::cout << "\nResult: " << "£" << amount << " = $" << amount * 1.12 << '\n'; break;
+    case(3):
+      std::cout << "\nResult: " << "¥" << amount << " = $" << amount * 0.067 << '\n'; break;
+    case(4):
+      std::cout << "\nResult: " << "₩" << amount << " = $" << amount * 0.00070 << '\n'; break;
+    case(5):
+      std::cout << "\nResult: " << "dkk" << amount << " = $" << amount * 0.13 << '\n'; break;
+    default:
+      std::cout << "\nSorry, I am unable to convert for that currency.\n"; break;
+  }
 
+  return 0;
 }
