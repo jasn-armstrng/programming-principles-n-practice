8f119e01433825c3c64ec7d462715441d69bdeaf
Jason Armstrong <jasnarmstrng@gmail.com>
2022-11-03 18:12:32 +0000

    feat: Add output for result to user

--- pt-1-the-basics/chp-4-computation/exercises/src/sq_by_addition.cpp
+++ pt-1-the-basics/chp-4-computation/exercises/src/sq_by_addition.cpp
@@ -1,24 +1,29 @@
-// This program squares a number by repeated addition.
+// This program squares a +ve integer by repeated addition.
 
 #include <iostream>
 
-double square(double x)
+int square(int x)
 {
+  int sq = 0;
   for (int i = 0; i < x; ++i)
   {
-    x+=x;
+    sq+=x;
   }
-  return x;
+  return sq;
 }
 
 int main()
 {
-  double number;
+  int number;
   std::cout << "+----------------------+\n";
   std::cout << "| Squaring by addition |\n";
   std::cout << "+----------------------+\n\n";
 
   // Prompt for user values. Read in user input.
-  std::cout << "Please enter a number: ";
+  std::cout << "Please enter a +ve integer: ";
   std::cin >> number;
+
+  // Output square
+  std::cout << "\nSquare: " << square(number) << '\n';
+  return 0;
 }
