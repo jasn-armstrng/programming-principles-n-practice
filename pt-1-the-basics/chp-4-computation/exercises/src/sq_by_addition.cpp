804e3d874c2d1beb528a5f7dfb987a6193f5be95
Jason Armstrong <jasnarmstrng@gmail.com>
2022-11-03 17:57:24 +0000

    feat: Add prompt for user input

---
+++ pt-1-the-basics/chp-4-computation/exercises/src/sq_by_addition.cpp
@@ -0,0 +1,13 @@
+// This program squares a number by repeated addition.
+
+#include <iostream>
+int main()
+{
+  double number;
+  std::cout << "+----------------------+\n";
+  std::cout << "| Squaring by addition |\n";
+  std::cout << "+----------------------+\n\n";
+
+  // Prompt for user values. Read in user input.
+  std::cout << "Please enter a number: ";
+}
