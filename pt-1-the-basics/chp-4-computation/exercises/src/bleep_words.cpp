316eb6451a113572d99ad92eb504a5cccd88f76d
Jason Armstrong <jasnarmstrng@gmail.com>
2022-11-03 18:59:45 +0000

    feat: Add prompt for user input

--- pt-1-the-basics/chp-4-computation/exercises/src/bleep_words.cpp
+++ pt-1-the-basics/chp-4-computation/exercises/src/bleep_words.cpp
@@ -2,7 +2,16 @@
 // found in blacklist are written to output as BLEEP.
 
 #include <iostream>
+#include <vector>
 int main()
 {
+  std::vector<std::string> blacklist = {"crap"};
+  std::string current = "";
+  std::cout << "+----------------+\n";
+  std::cout << "| Bleep my words |\n";
+  std::cout << "+----------------+\n\n";
+
+  std::cout << "Note: To end this program press CTRL+D.\n";
+  std::cout << "Start writing: ";
 
 }
