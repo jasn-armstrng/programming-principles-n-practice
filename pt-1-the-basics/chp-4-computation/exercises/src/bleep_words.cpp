e2e11b93ccb93331658823272d2f7e0881b64993
Jason Armstrong <jasnarmstrng@gmail.com>
2022-11-03 19:34:15 +0000

    feat: Add check for blacklisted word

--- pt-1-the-basics/chp-4-computation/exercises/src/bleep_words.cpp
+++ pt-1-the-basics/chp-4-computation/exercises/src/bleep_words.cpp
@@ -5,13 +5,27 @@
 #include <vector>
 int main()
 {
-  std::vector<std::string> blacklist = {"crap"};
-  std::string current = "";
+  std::vector<std::string> blacklist = {"fox", "lazy", "dog"};
+  std::string censored = "";
+  std::string current;
   std::cout << "+----------------+\n";
   std::cout << "| Bleep my words |\n";
   std::cout << "+----------------+\n\n";
 
-  std::cout << "Note: To end this program press CTRL+D.\n";
-  std::cout << "Start writing: ";
+  std::cout << "Enter your text to censor at > below.\n";
+  std::cout << "When you are done press Enter then ctrl+D for results.\n";
+  std::cout << "\n> ";
 
+  while (std::cin >> current)
+  {
+    for(std::string w:blacklist)
+    {
+      if (current == w)
+        current = "BLEEP";
+    }
+    censored += (current + ' ');
+  }
+
+  std::cout << "\n\nCensored text: "<< censored << '\n';
+  return 0;
 }
