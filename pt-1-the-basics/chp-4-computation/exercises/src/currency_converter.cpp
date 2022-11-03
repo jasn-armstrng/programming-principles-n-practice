0d4313c996551f9ebeace055a79867b006121f57
Jason Armstrong <jasnarmstrng@gmail.com>
2022-11-03 11:36:46 +0000

    Create new file currency_converter.cpp

---
+++ pt-1-the-basics/chp-4-computation/exercises/src/currency_converter.cpp
@@ -0,0 +1,14 @@
+/*
+  This program converts currency values to USD.
+
+  How it works:
+  1. Input: Prompt user to enter a currency (symbol) immediately followed by an amount,
+     Ex. €2, £10, ¥8
+  2. Output: "€2 = $1.70"
+*/
+
+#include <iostream>
+int main()
+{
+
+}
