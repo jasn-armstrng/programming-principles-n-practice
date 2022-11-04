6268f9c508a119f396fac5963ac2d8872503934b
Jason Armstrong <jasnarmstrng@gmail.com>
2022-11-04 11:26:21 +0000

    feat: Add min function

--- pt-1-the-basics/chp-4-computation/drills/src/ch_4_drill.cpp
+++ pt-1-the-basics/chp-4-computation/drills/src/ch_4_drill.cpp
@@ -2,9 +2,19 @@
 This program completes Chapter 4's Drill on pg. 126
 1. - Read in two integers and print them to console.
    - Exit program when a '|' is entered
+2. - Write out the "the smaller value is: " << number
+   - Write out the "the larger value is: " << number
 */
 
 #include <iostream>
+
+int min(int x, int y)
+{
+  if (x > y)
+    return y;
+  return x;
+}
+
 int main()
 {
   int num1, num2;
@@ -13,6 +23,7 @@ int main()
   std::cout << "Please enter two integers. Enter a '|' to terminate the program.\n\nIntegers: ";
   while (std::cin >> num1 >> num2)
   {
+    (num1 > num2) ? std::cout << num1 << " is larger than " << num2 << '\n';
     std::cout << "You entered: " << num1 << ' ' << num2 << "\n\nIntegers:";
   }
   return 0;
