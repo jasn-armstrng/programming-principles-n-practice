47b530cc12e719e086d18a7fb36b20ac8282be37
Jason Armstrong <jasnarmstrng@gmail.com>
2022-11-04 11:13:03 +0000

    feat: Add prompt for user input. Add read in and output

--- pt-1-the-basics/chp-4-computation/drills/src/ch_4_drill.cpp
+++ pt-1-the-basics/chp-4-computation/drills/src/ch_4_drill.cpp
@@ -1,4 +1,5 @@
 /*
+This program completes Chapter 4's Drill on pg. 126
 1. - Read in two integers and print them to console.
    - Exit program when a '|' is entered
 */
@@ -9,10 +10,10 @@ int main()
   int num1, num2;
 
   // Prompt user for input
-  std::cout << "Please enter two integers: ";
+  std::cout << "Please enter two integers. Enter a '|' to terminate the program.\n\nIntegers: ";
   while (std::cin >> num1 >> num2)
   {
-    std::cout << num1 << ' ' << num2 << '\n';
+    std::cout << "You entered: " << num1 << ' ' << num2 << "\n\nIntegers:";
   }
   return 0;
 }
