1d35a8108a6cc38249c32df2e1f2cf5d5015b15d
Jason Armstrong <jasnarmstrng@gmail.com>
2022-11-05 23:38:32 +0000

    feat: Add function mean - calculates mean

--- pt-1-the-basics/chp-4-computation/exercises/src/calc_mean_median.cpp
+++ pt-1-the-basics/chp-4-computation/exercises/src/calc_mean_median.cpp
@@ -2,15 +2,35 @@
 
 #include <iostream>
 #include <vector>
+
+double mean(std::vector<double> v)
+{
+  double sum = 0;
+  for (double i:v) // Note the type of the iterator should match the type of the element.
+  {
+    sum+=i;
+  }
+  return sum/v.size();
+}
+
 int main()
 {
   std::vector<double> temps;
-  double values;
 
   std::cout << "+----------------------+\n";
   std::cout << "| Calc Mean and Median |\n";
   std::cout << "+----------------------+\n\n";
 
-  std::cout << "Please enter a sequence of temperature values\n";
+  std::cout << "Please enter a sequence of temperature values. A | signals the end of the sequence.\n";
   std::cout << "Values: ";
+
+  for(double temp; std::cin >> temp;) // Note the syntax for reading values in with for. The ;
+  {
+    temps.push_back(temp);
+  }
+
+  // Compute mean
+  std::cout << "Mean: "<< mean(temps) << '\n';
+
+  return 0;
 }
