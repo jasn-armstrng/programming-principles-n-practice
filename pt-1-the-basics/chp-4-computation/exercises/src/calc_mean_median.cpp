92b7ee224df7a16946be39f9401cadc502640d58
Jason Armstrong <jasnarmstrng@gmail.com>
2022-11-05 23:12:36 +0000

    feat: Add prompt for user input

--- pt-1-the-basics/chp-4-computation/exercises/src/calc_mean_median.cpp
+++ pt-1-the-basics/chp-4-computation/exercises/src/calc_mean_median.cpp
@@ -1,9 +1,16 @@
 // This program calculates the mean and median of a sequence of temperatures
 
 #include <iostream>
+#include <vector>
 int main()
 {
+  std::vector<double> temps;
+  double values;
+
   std::cout << "+----------------------+\n";
   std::cout << "| Calc Mean and Median |\n";
   std::cout << "+----------------------+\n\n";
+
+  std::cout << "Please enter a sequence of temperature values\n";
+  std::cout << "Values: ";
 }
