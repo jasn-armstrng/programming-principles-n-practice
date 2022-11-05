9bb729218e195e4ef82f1275a8aeafa18873cc42
Jason Armstrong <jasnarmstrng@gmail.com>
2022-11-05 23:07:43 +0000

    Create new file calc_mean_median.cpp

---
+++ pt-1-the-basics/chp-4-computation/exercises/src/calc_mean_median.cpp
@@ -0,0 +1,9 @@
+// This program calculates the mean and median of a sequence of temperatures
+
+#include <iostream>
+int main()
+{
+  std::cout << "+----------------------+\n";
+  std::cout << "| Calc Mean and Median |\n";
+  std::cout << "+----------------------+\n\n";
+}
