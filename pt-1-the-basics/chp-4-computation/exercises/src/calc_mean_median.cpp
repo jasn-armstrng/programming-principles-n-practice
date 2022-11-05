b7fadc4fed0f6f2b913458111b5e4552d69b47f0
Jason Armstrong <jasnarmstrng@gmail.com>
2022-11-05 23:57:13 +0000

    feat: Add function median - calculates median

--- pt-1-the-basics/chp-4-computation/exercises/src/calc_mean_median.cpp
+++ pt-1-the-basics/chp-4-computation/exercises/src/calc_mean_median.cpp
@@ -1,16 +1,23 @@
 // This program calculates the mean and median of a sequence of temperatures
 
+#include <algorithm>
 #include <iostream>
 #include <vector>
 
 double mean(std::vector<double> v)
 {
   double sum = 0;
-  for (double i:v) // Note the type of the iterator should match the type of the element.
-  {
-    sum+=i;
-  }
+  for (double i:v) sum+=i; // Note the type of the iterator should match the type of the element.
   return sum/v.size();
+}
+
+double median(std::vector<double> v)
+{
+  std::sort(v.begin(), v.end());
+  if (v.size()%2 != 0)
+  {
+    return v[(v.size()/2)];
+  }
 }
 
 int main()
@@ -24,13 +31,20 @@ int main()
   std::cout << "Please enter a sequence of temperature values. A | signals the end of the sequence.\n";
   std::cout << "Values: ";
 
-  for(double temp; std::cin >> temp;) // Note the syntax for reading values in with for. The ;
-  {
-    temps.push_back(temp);
-  }
+  // Note the syntax below for reading values in with for. The ; after each statement
+  for(double temp; std::cin >> temp;) temps.push_back(temp);
 
   // Compute mean
-  std::cout << "Mean: "<< mean(temps) << '\n';
+  std::cout << "Mean: "<< mean << '\n';
+
+  // Compute median
+  std::sort(temps.begin(), temps.end());
+  for(double i:temps)
+  {
+    std::cout << i << '\n';
+  }
+
+  std::cout << "Median: "<< median(temps) << '\n';
 
   return 0;
 }
