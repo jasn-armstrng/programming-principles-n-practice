1710d5b23b0aa34fda93e6217d5fb88426fa5306
Jason Armstrong <jasnarmstrng@gmail.com>
2022-11-06 00:28:01 +0000

    refactor: Update output dialog

--- pt-1-the-basics/chp-4-computation/exercises/src/calc_mean_median.cpp
+++ pt-1-the-basics/chp-4-computation/exercises/src/calc_mean_median.cpp
@@ -1,4 +1,5 @@
-// This program calculates the mean and median of a sequence of temperatures
+// This program calculates the mean and median of a sequence of values e.g
+// temperatures.
 
 #include <algorithm>
 #include <iostream>
@@ -7,7 +8,7 @@
 double mean(std::vector<double> v)
 {
   double sum = 0;
-  for (double i:v) sum+=i; // Note the type of the iterator should match the type of the element.
+  for (double i:v) sum+=i; // Note: type of the iterator matches element type.
   return sum/v.size();
 }
 
@@ -24,21 +25,22 @@ double median(std::vector<double> v)
 
 int main()
 {
-  std::vector<double> temps;
+  std::vector<double> vec;
 
   std::cout << "+----------------------+\n";
   std::cout << "| Calc Mean and Median |\n";
   std::cout << "+----------------------+\n\n";
 
-  std::cout << "Please enter a sequence of temperature values. A | signals the end of the sequence.\n";
+  std::cout << "Please enter a sequence of values. Use | to signal the end of the sequence.\n";
   std::cout << "Values: ";
 
   // Note the syntax below for reading values in with for. The ; after each statement
-  for(double temp; std::cin >> temp;) temps.push_back(temp);
+  for(double val; std::cin >> val;) vec.push_back(val);
 
-  // Compute mean
-  std::cout << "Mean: "<< mean(temps) << '\n';
-  std::cout << "Median: "<< median(temps) << '\n';
+  std::cout << "\nResults:\n";
+  std::cout << "- No. of Values: " << vec.size() << '\n';
+  std::cout << "- Mean: "<< mean(vec) << '\n';
+  std::cout << "- Median: "<< median(vec) << '\n';
 
   return 0;
 }
