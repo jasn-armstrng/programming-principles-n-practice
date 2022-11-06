e1cb9733e13432dbb5bf04faa0ba5f6530ba27d5
Jason Armstrong <jasnarmstrng@gmail.com>
2022-11-06 21:13:39 +0000

    Create new file number_guessing.cpp

---
+++ pt-1-the-basics/chp-4-computation/exercises/src/number_guessing.cpp
@@ -0,0 +1,10 @@
+// This program uses binary search to determine the number the player has chosen
+
+#include <iostream>
+int main()
+{
+  std::cout << "+----------------+\n";
+  std::cout << "| Number Guesser |\n";
+  std::cout << "+----------------+\n\n";
+
+}
---
+++ pt-1-the-basics/chp-4-computation/exercises/src/pick_a_number.cpp
@@ -0,0 +1,10 @@
+// This program uses binary search to 'guess' the number the player has chosen
+
+#include <iostream>
+int main()
+{
+  std::cout << "+-----------------+\n";
+  std::cout << "| Number Guessing |\n";
+  std::cout << "+-----------------+\n\n";
+
+}
