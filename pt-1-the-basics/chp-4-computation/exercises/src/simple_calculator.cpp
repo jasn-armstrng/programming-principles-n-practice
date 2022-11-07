564e1bc3cb9e4d8a89e569aada84fa1394c72146
Jason Armstrong <jasnarmstrng@gmail.com>
2022-11-07 12:48:05 +0000

    feat: Add greeting and user input prompt

--- pt-1-the-basics/chp-4-computation/exercises/src/simple_calculator.cpp
+++ pt-1-the-basics/chp-4-computation/exercises/src/simple_calculator.cpp
@@ -12,5 +12,13 @@ int main()
   std::cout << "| Simple Calculator |\n";
   std::cout << "+-------------------+\n\n";
 
+  std::cout << "Hello User!\n";
+  std::cout << "\nPlease enter your calculation to be performed e.g. 35.6 24.1 + =\n";
+  std::cout << "Calculate: ";
+
+  while (std::cin >> num1 >> num2 >> operation)
+  {
+    std::cout << num1 << ' ' << num2 << ' ' << operation << '\n';
+  }
   return 0;
 }
