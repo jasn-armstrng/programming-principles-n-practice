f168a532b83a432679504fa386d31a213a2263be
Jason Armstrong <jasnarmstrng@gmail.com>
2022-11-07 15:22:52 +0000

    docs: Add note for type conversion of vector.size()

--- pt-1-the-basics/chp-4-computation/exercises/src/integer_to_word.cpp
+++ pt-1-the-basics/chp-4-computation/exercises/src/integer_to_word.cpp
@@ -22,7 +22,7 @@ int main()
   std::cout << "\nInput: ";
   while(std::cin >> num)
   {
-    if (num < 0 || num >= int(integer_spelling.size()))
+    if (num < 0 || num >= int(integer_spelling.size())) // Reason for type conversion below
     {
       std::cout << "I'm sorry I don't know that one.\n";
       std::cout << "\nInput: ";
@@ -36,3 +36,9 @@ int main()
   std::cout << "\nThanks for using Integer to Word :)\n";
   return 0;
 }
+
+// integer_to_word.cpp:22:24:
+// warning: comparison of integers of different signs: 'int'
+// and 'std::vector<std::string>::size_type' (aka 'unsigned long') [-Wsign-compare]
+
+//     if (num < 0 || num >= integer_spelling.size())
