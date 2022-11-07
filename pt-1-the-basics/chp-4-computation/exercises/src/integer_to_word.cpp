5cbe5bb61f0977415b7ddd0965654a9dbdd62e96
Jason Armstrong <jasnarmstrng@gmail.com>
2022-11-07 13:46:15 +0000

    Create new file integer_to_word.cpp

---
+++ pt-1-the-basics/chp-4-computation/exercises/src/integer_to_word.cpp
@@ -0,0 +1,12 @@
+// This program converts integer input to their spelled word equivalent e.g.
+// In: 7 Out: seven
+
+#include <iostream>
+int main()
+{
+  std::cout << "+-----------------+\n";
+  std::cout << "| Integer to Word |\n";
+  std::cout << "+-----------------+\n\n";
+
+  std::cout << "Hello User!\n";
+}
