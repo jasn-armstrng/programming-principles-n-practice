55de38658604ad012dc49252415735764a722d6c
Jason Armstrong <jasnarmstrng@gmail.com>
2022-11-07 14:01:06 +0000

    feat: Add opening greeting and user input prompt

--- pt-1-the-basics/chp-4-computation/exercises/src/integer_to_word.cpp
+++ pt-1-the-basics/chp-4-computation/exercises/src/integer_to_word.cpp
@@ -2,11 +2,25 @@
 // In: 7 Out: seven
 
 #include <iostream>
+#include <vector>
 int main()
 {
+  std::vector<std::string> integer_spelling
+    = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+  int num;
+
   std::cout << "+-----------------+\n";
   std::cout << "| Integer to Word |\n";
   std::cout << "+-----------------+\n\n";
 
   std::cout << "Hello User!\n";
+  std::cout << "\nProgram usage:\n- Input: 7 Output: seven\n";
+
+  std::cout << "\nInput: ";
+  while(std::cin >> num)
+  {
+    std::cout << " Output: " << integer_spelling[num] << '\n';
+  }
+
+  return 0;
 }
