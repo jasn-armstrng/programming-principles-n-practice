673395ce4c1d71423197c9590411c43c110d8dda
Jason Armstrong <jasnarmstrng@gmail.com>
2022-11-07 19:37:11 +0000

    Create new file integer_to_word_v2.cpp

--- pt-1-the-basics/chp-4-computation/exercises/src/integer_to_word.cpp
+++ pt-1-the-basics/chp-4-computation/exercises/src/integer_to_word.cpp
@@ -1,8 +1,5 @@
-// This program,
-// 1. Converts integer input to their spelled word equivalent e.g.
-//  In: 7 Out: seven
-// 2. Converts a spelled word integer to integer
-//  In: seven Out: 7
+// This program converts integer input to their spelled word equivalent e.g.
+// In: 7 Out: seven
 
 #include <iostream>
 #include <vector>
@@ -10,22 +7,31 @@ int main()
 {
   std::vector<std::string> integer_spelling
     = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-  std::string num;
+  int num;
 
-  std::cout << "+------------------+\n";
-  std::cout << "| Integer <-> Word |\n";
-  std::cout << "+------------------+\n\n";
+  std::cout << "+-----------------+\n";
+  std::cout << "| Integer to Word |\n";
+  std::cout << "+-----------------+\n\n";
 
   std::cout << "Hello User!\n";
   std::cout << "\nHow to use this program:";
   std::cout << "\n------------------------";
-  std::cout << "\n- At Input: below enter for e.g. 7 or seven";
+  std::cout << "\n- At Input: below enter an integer e.g. 7";
   std::cout << "\n- To exit enter the | symbol.\n";
 
   std::cout << "\nInput: ";
   while(std::cin >> num)
   {
-
+    if (num < 0 || num >= int(integer_spelling.size())) // Reason for type conversion below
+    {
+      std::cout << "I'm sorry I don't know that one.\n";
+      std::cout << "\nInput: ";
+    }
+    else
+    {
+      std::cout << "Output: " << integer_spelling[num] << '\n';
+      std::cout << "\nInput: ";
+    }
   }
   std::cout << "\nThanks for using Integer to Word :)\n";
   return 0;
