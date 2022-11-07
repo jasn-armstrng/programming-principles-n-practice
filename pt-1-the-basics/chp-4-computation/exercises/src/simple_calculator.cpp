8c7390db539f5e7476fa2311099a064e137c4b09
Jason Armstrong <jasnarmstrng@gmail.com>
2022-11-07 12:37:21 +0000

    Create new file simple_calculator.cpp

---
+++ pt-1-the-basics/chp-4-computation/exercises/src/simple_calculator.cpp
@@ -0,0 +1,16 @@
+// This program performs as a very simple calculator. It takes two real numbers
+// and an operator as input e.g. 35.6 24.1 + and returns the result of the
+// operation.
+
+#include <iostream>
+int main()
+{
+  double num1, num2;
+  char operation;
+
+  std::cout << "+-------------------+\n";
+  std::cout << "| Simple Calculator |\n";
+  std::cout << "+-------------------+\n\n";
+
+  return 0;
+}
