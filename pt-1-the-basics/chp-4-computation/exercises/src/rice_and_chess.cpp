99abdd2c9b08381c3c3190e87783a8069d31164a
Jason Armstrong <jasnarmstrng@gmail.com>
2022-11-08 14:54:56 +0000

    Create new file rice_and_chess.cpp

---
+++ pt-1-the-basics/chp-4-computation/exercises/src/rice_and_chess.cpp
@@ -0,0 +1,13 @@
+// There is an old story that the inventor of chess asked for his reward -
+// a grain of rice on the first square of the chess board, 2 on the second,
+// 4 on the third and so on ...
+// This program computes the number of squares needed for 10^3, 10^6 and 10^9
+// grains of rice.
+
+#include <iostream>
+int main()
+{
+  std::cout << "+----------------+\n";
+  std::cout << "| Rice and Chess |\n";
+  std::cout << "+----------------+\n\n";
+}
