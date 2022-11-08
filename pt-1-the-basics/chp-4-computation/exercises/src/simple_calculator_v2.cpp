9126e10753e5a1c61939d76b779e9b666d20c71a
Jason Armstrong <jasnarmstrng@gmail.com>
2022-11-08 04:43:21 +0000

    feat: Add word_to_int function

--- pt-1-the-basics/chp-4-computation/exercises/src/simple_calculator_v2.cpp
+++ pt-1-the-basics/chp-4-computation/exercises/src/simple_calculator_v2.cpp
@@ -7,10 +7,29 @@
 
 #include <iostream>
 #include <vector>
-int main()
+
+int word_to_int(std::string word)
 {
   std::vector<std::string> ints_as_words
     = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+  for(int i = 0; i < int(ints_as_words.size()); ++i)
+    {
+      if(word == ints_as_words[i])
+      {
+        return i;
+      }
+      else if (word == std::to_string(i))
+      {
+        return i;
+      }
+    }
+  return 0;
+}
+
+int main()
+{
+
   std::string int1, int2, operation;
 
   std::cout << "+----------------------+\n";
@@ -21,9 +40,14 @@ int main()
   std::cout << "\nHow to use this program:\n";
   std::cout << "- Input examples,\n";
   std::cout << "  6 7 +\n  7 8 *\n  five four -\n  nine three /\n";
+  std::cout << "- Single digits only\n";
   std::cout << "- To exit program enter ctrl-D\n";
 
   std::cout << "Input: ";
-
+  while (std::cin >> int1 >> int2 >> operation)
+  {
+    // std::cout << int1 << ' ' << int2 << ' ' << operation << '\n';
+    std::cout << word_to_int(int1) << ' ' << word_to_int(int2) << ' ' << operation << '\n';
+  }
   return 0;
 }
