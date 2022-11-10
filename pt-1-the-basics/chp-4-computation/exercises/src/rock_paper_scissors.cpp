afcdee607e78045c823407d8a8a2ef6eee29c3c3
Jason Armstrong <jasnarmstrng@gmail.com>
2022-11-10 19:42:26 +0000

    feat: Add pseudo-random number generator

--- pt-1-the-basics/chp-4-computation/exercises/src/rock_paper_scissors.cpp
+++ pt-1-the-basics/chp-4-computation/exercises/src/rock_paper_scissors.cpp
@@ -9,7 +9,9 @@ int main()
   // List of moves computer randomly chooses from
   std::vector<std::string> moves = {"rock", "paper", "scissors", "rock", "paper", "scissors", "rock", "paper", "scissors"};
 
-
+  // Next 2 lines make up our psuedo-random number generator
+  std::random_device ran_dev;
+  std::uniform_int_distribution<int> dist(0,8);
 
   std::string player_move;
   std::string computer_move;
