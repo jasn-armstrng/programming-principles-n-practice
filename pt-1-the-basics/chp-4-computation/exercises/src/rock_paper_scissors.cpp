cfc60b50887670ce4b4bff806aeb9d6e621d9eea
Jason Armstrong <jasnarmstrng@gmail.com>
2022-11-10 12:19:24 +0000

    Create new file rock_paper_scissors.cpp

---
+++ pt-1-the-basics/chp-4-computation/exercises/src/rock_paper_scissors.cpp
@@ -0,0 +1,11 @@
+// This program plays the game "Rock, Paper, Scissors"
+
+#include <iostream>
+int main()
+{
+  std::cout << "+-----------------------+\n";
+  std::cout << "| Rock, Paper, Scissors |\n";
+  std::cout << "+-----------------------+\n\n";
+
+  return 0;
+}
