40d93bf03bc53ed23c25bed3c6f37d0ddfafb01b
Jason Armstrong <jasnarmstrng@gmail.com>
2022-11-10 19:41:29 +0000

    feat: Add prompt for player move

--- pt-1-the-basics/chp-4-computation/exercises/src/rock_paper_scissors.cpp
+++ pt-1-the-basics/chp-4-computation/exercises/src/rock_paper_scissors.cpp
@@ -1,11 +1,29 @@
 // This program plays the game "Rock, Paper, Scissors"
 
 #include <iostream>
+#include <random>
+#include <vector>
+
 int main()
 {
+  // List of moves computer randomly chooses from
+  std::vector<std::string> moves = {"rock", "paper", "scissors", "rock", "paper", "scissors", "rock", "paper", "scissors"};
+
+
+
+  std::string player_move;
+  std::string computer_move;
+  char play_again;
+
   std::cout << "+-----------------------+\n";
   std::cout << "| Rock, Paper, Scissors |\n";
   std::cout << "+-----------------------+\n\n";
 
+  std::cout << "Your move: ";
+  while (std::cin >> player_move)
+  {
+
+  }
+  std::cout << "\nThanks for playing!\n";
   return 0;
 }
