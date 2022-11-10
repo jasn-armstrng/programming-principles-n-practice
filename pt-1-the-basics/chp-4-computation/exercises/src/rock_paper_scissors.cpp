10f817a2e1e1308a14b77f8d357e4bb93adca378
Jason Armstrong <jasnarmstrng@gmail.com>
2022-11-10 19:43:45 +0000

    feat: Add determine outcome function w/ easter egg

--- pt-1-the-basics/chp-4-computation/exercises/src/rock_paper_scissors.cpp
+++ pt-1-the-basics/chp-4-computation/exercises/src/rock_paper_scissors.cpp
@@ -4,6 +4,33 @@
 #include <random>
 #include <vector>
 
+std::string outcome(std::string p_move, std::string c_move)
+{
+  if (p_move == c_move)
+    return "Draw!";
+
+  if (p_move == "rock" && c_move == "scissors")
+    return "Player wins!";
+  else if (c_move == "rock" && p_move == "scissors")
+    return "Computer wins!";
+
+  if (p_move == "paper" && c_move == "rock")
+    return "Player wins!";
+  else if (c_move == "paper" && p_move == "rock")
+    return "Computer wins!";
+
+  if (p_move == "scissors" && c_move == "paper")
+    return "Player wins!";
+  else if (c_move == "scissors" && p_move == "paper")
+    return "Computer wins!";
+
+  // Easter egg
+  if (p_move == "love")
+    return "Love conquers all!";
+
+  return "No result!";
+}
+
 int main()
 {
   // List of moves computer randomly chooses from
