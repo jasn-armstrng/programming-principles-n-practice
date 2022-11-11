6786f2ee4f7766f3c8e10542a5cb63e5c76d5fab
Jason Armstrong <jasnarmstrng@gmail.com>
2022-11-11 11:52:01 +0000

    feat: Add prompt for user to enter prime limiter

--- pt-1-the-basics/chp-4-computation/exercises/src/finding_primes.cpp
+++ pt-1-the-basics/chp-4-computation/exercises/src/finding_primes.cpp
@@ -1,7 +1,13 @@
-// This program uses the "Sieve of Erastosthones" to find primes up to a max val
+// This program uses the "Sieve of Erastosthones" to find primes below a max val
 
 #include <iostream>
 #include <vector>
+
+int sieve_of_eras(int max_val)
+{
+  return 0;
+}
+
 int main()
 {
   std::vector<int> primes;
@@ -11,7 +17,7 @@ int main()
   std::cout << "| Finding Primes |\n";
   std::cout << "+----------------+\n\n";
 
-  std::cout << "Find primes up to: ";
+  std::cout << "Find all primes below: ";
   std::cin >> max;
 
   return 0;
