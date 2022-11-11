ff8588d38c050af55db7f15f33c903aa8af7a4b5
Jason Armstrong <jasnarmstrng@gmail.com>
2022-11-11 19:04:28 +0000

    Create new file first_n_primes.cpp

---
+++ pt-1-the-basics/chp-4-computation/exercises/src/first_n_primes.cpp
@@ -0,0 +1,13 @@
+// This program takes an input n and finds the first n primes
+
+#include <iostream>
+int main()
+{
+  int n; // number of primes
+
+  std::cout << "+----------------+\n";
+  std::cout << "| First N Primes |\n";
+  std::cout << "+----------------+\n\n";
+
+  return 0;
+}
