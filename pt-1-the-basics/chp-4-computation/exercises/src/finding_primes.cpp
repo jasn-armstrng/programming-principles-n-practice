1742afa7339ffc1ec0718c91f07791f7707881bb
Jason Armstrong <jasnarmstrng@gmail.com>
2022-11-11 11:40:58 +0000

    Create new file finding_primes.cpp

---
+++ pt-1-the-basics/chp-4-computation/exercises/src/finding_primes.cpp
@@ -0,0 +1,18 @@
+// This program uses the "Sieve of Erastosthones" to find primes up to a max val
+
+#include <iostream>
+#include <vector>
+int main()
+{
+  std::vector<int> primes;
+  int max;
+
+  std::cout << "+----------------+\n";
+  std::cout << "| Finding Primes |\n";
+  std::cout << "+----------------+\n\n";
+
+  std::cout << "Find primes up to: ";
+  std::cin >> max;
+
+  return 0;
+}
