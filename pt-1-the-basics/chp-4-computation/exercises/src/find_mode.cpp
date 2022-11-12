00f61dc7b5058da096d8449d4de39c6b3b975853
Jason Armstrong <jasnarmstrng@gmail.com>
2022-11-12 21:15:35 +0000

    feat: Add mode function

--- pt-1-the-basics/chp-4-computation/exercises/src/find_mode.cpp
+++ pt-1-the-basics/chp-4-computation/exercises/src/find_mode.cpp
@@ -1,63 +1,61 @@
-// This program finds the mode of a set of integers
+// This program finds the mode of a set of integers.
+
 
 #include <algorithm>
 #include <iostream>
 #include <vector>
 
-double max(double x, double y)
+int mode(std::vector<int> v)
 {
-  if (x>y)
-    return x;
-  return y;
-}
+  int max_value = 0, // Mode
+      max_count = 0; // Tracks the max frequency of a value in the vector
 
-int mode(std::vector<int> vec)
-{
-  int mode;
-  int max_frequency = 1;
-  int counter = 0;
-  // mode is the value in a sequence that occurs most
-  std::sort(vec.begin(), vec.end());
+  std::sort(v.begin(), v.end());  // Sort vector ascending
 
-  for (int i = 1; i < int(vec.size()); ++i)
-  {
-    if (vec[i] == vec[i-1])
+  for(int i = 0; i < int(v.size()); ++i)
+  { // This nested iteration works because the mode does not beat
+    // the count it got on first iteration thru the inner for.
+    int count = 0; // Tracks the consecutive count of a value
+                   // reset to 0 for each iteration/value
+    for(int j = i; j < int(v.size()); ++j)
     {
-      ++counter;
+      if(v[j] == v[i])
+      {
+        ++count;
+      }
     }
-    else
+    if (count > max_count)
     {
-
+      // update the mode and frequency
+      max_value = v[i];
+      max_count = count;
     }
-
   }
+  if (max_count > 1)
+    return max_value;
 
-
+  return -1; // If no repeated numbers in a set of elements > 0, then no mode
 }
 
 int main()
 {
-  int integer;
   std::vector<int> integers;
+
   std::cout << "+-----------+\n";
   std::cout << "| Find Mode |\n";
   std::cout << "+-----------+\n\n";
 
   std::cout << "\nHow this program works:";
-  std::cout << "\n---------------------------------------------";
-  std::cout << "\n- Enter sequence of space separated integers.";
-  std::cout << "\n- End set of integers with a | ";
-  std::cout << "\n- To exit the program enter - .";
-  std::cout << "\n---------------------------------------------";
-
+  std::cout << "\n--------------------------------------------";
+  std::cout << "\n- Enter sequence of space separated integers";
+  std::cout << "\n- End set of integers with a |";
+  std::cout << "\n- If no mode program will output -1";
+  std::cout << "\n- To exit the program enter |";
+  std::cout << "\n--------------------------------------------";
+  // User in
   std::cout << "\n\nIntegers: ";
-  while (std::cin >> integer)
-  {
-    integers.push_back(integer);
-  }
-  // for (int x: integers)
-  // {
-  //   std::cout << x << '\n';
-  // }
+  for(double val; std::cin >> val;) integers.push_back(val);
+  // Program out
+  std::cout << "\nMode: " << mode(integers) << '\n';
   return 0;
 }
