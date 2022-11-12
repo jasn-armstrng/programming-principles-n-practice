ad9f9421f78c2ced6f927421dd5ce97f633c2394
Jason Armstrong <jasnarmstrng@gmail.com>
2022-11-12 17:29:18 +0000

    Create new file find_mode.cpp

---
+++ pt-1-the-basics/chp-4-computation/exercises/src/find_mode.cpp
@@ -0,0 +1,14 @@
+// This program finds the mode of a set of integers
+
+#include <iostream>
+#include <vector>
+
+int main()
+{
+  int integer;
+  std::vector<int> integers;
+  std::cout << "+-----------+\n";
+  std::cout << "| Find Mode |\n";
+  std::cout << "+-----------+\n\n";
+
+}
