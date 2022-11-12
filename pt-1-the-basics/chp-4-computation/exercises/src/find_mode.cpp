c98461b31a335029bb71df69829e4fa0473355c9
Jason Armstrong <jasnarmstrng@gmail.com>
2022-11-12 18:15:28 +0000

    feat: Add max function

--- pt-1-the-basics/chp-4-computation/exercises/src/find_mode.cpp
+++ pt-1-the-basics/chp-4-computation/exercises/src/find_mode.cpp
@@ -1,7 +1,39 @@
 // This program finds the mode of a set of integers
 
+#include <algorithm>
 #include <iostream>
 #include <vector>
+
+double max(double x, double y)
+{
+  if (x>y)
+    return x;
+  return y;
+}
+
+int mode(std::vector<int> vec)
+{
+  int mode;
+  int max_frequency = 1;
+  int counter = 0;
+  // mode is the value in a sequence that occurs most
+  std::sort(vec.begin(), vec.end());
+
+  for (int i = 1; i < int(vec.size()); ++i)
+  {
+    if (vec[i] == vec[i-1])
+    {
+      ++counter;
+    }
+    else
+    {
+
+    }
+
+  }
+
+
+}
 
 int main()
 {
@@ -12,15 +44,20 @@ int main()
   std::cout << "+-----------+\n\n";
 
   std::cout << "\nHow this program works:";
-  std::cout << "\n----------------------------------------";
-  std::cout << "\n- Enter set of space separated integers.";
+  std::cout << "\n---------------------------------------------";
+  std::cout << "\n- Enter sequence of space separated integers.";
   std::cout << "\n- End set of integers with a | ";
   std::cout << "\n- To exit the program enter - .";
-  std::cout << "\n----------------------------------------";
+  std::cout << "\n---------------------------------------------";
 
   std::cout << "\n\nIntegers: ";
   while (std::cin >> integer)
   {
     integers.push_back(integer);
   }
+  // for (int x: integers)
+  // {
+  //   std::cout << x << '\n';
+  // }
+  return 0;
 }
