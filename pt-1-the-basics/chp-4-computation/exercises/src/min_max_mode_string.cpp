bac43199e08621f34b4fc6c01bb8148349f4635f
Jason Armstrong <jasnarmstrng@gmail.com>
2022-11-13 15:17:53 +0000

    refactor: Remove unused min and max functions

--- pt-1-the-basics/chp-4-computation/exercises/src/min_max_mode_string.cpp
+++ pt-1-the-basics/chp-4-computation/exercises/src/min_max_mode_string.cpp
@@ -5,9 +5,9 @@ Definitions:
  max: longest length string
  mode: string that appears most frequently
 
- 1. Find min string: Store the first string and its length in an vector
+ 1. Find min string: Store the first string index and its length in an vector
     of size 2. Compare the length of each subsequent string with the length in
-    the vector. If less then that string and length are the new values in
+    the vector. If less then that string index and length are the new values in
     vector.
  2. Find max string: Same as 1 but if length is greater then we update the
     vector with the new values.
@@ -17,13 +17,10 @@ Definitions:
 #include <iostream>
 #include <vector>
 
-int min(int x, int y){ return (x>y) ? y : x; }
-int max(int x, int y){ return (x>y) ? x : y; }
-
 int main()
 {
   std::vector<std::string> strings;
-  std::string str;
+  std::string str, min_string, max_string;
 
   std::cout << "+---------------------------+\n";
   std::cout << "| Min, Max, Mode of Strings |\n";
@@ -31,6 +28,21 @@ int main()
 
   std::cout << "Enter a sequence of strings: ";
   for(std::string str; std::cin>>str;) strings.push_back(str);
+  min_string = strings[0], max_string = strings[1];
 
+  for(std::string s: strings)
+  {
+    if (int(s.size()) < int(min_string.size()) && s < min_string)
+      min_string = s;
+
+    if (int(s.size()) > int(max_string.size()) && s > max_string)
+      max_string = s;
+  }
+
+  // std::cout << "Min: " << min_string << " Max: " << max_string <<'\n';
+  // std::string s1 = "is",
+  //             s2 = "be";
+  // bool comp = s1 < s2;
+  // std::cout << comp << '\n';
   return 0;
 }
