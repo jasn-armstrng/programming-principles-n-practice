8b404b12c81025619bf08f49c8a6655952da659c
Jason Armstrong <jasnarmstrng@gmail.com>
2022-11-13 13:00:09 +0000

    Create new file min_max_mode_string.cpp

---
+++ pt-1-the-basics/chp-4-computation/exercises/src/min_max_mode_string.cpp
@@ -0,0 +1,14 @@
+/*
+This programs finds the min, max, and mode of a sequence of strings.
+Definitions:
+ min: shortest length string
+ max: longest length string
+ mode: string that appears most frequently
+*/
+
+#include <iostream>
+#include <vector>
+int main()
+{
+
+}
