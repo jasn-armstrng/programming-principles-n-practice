601b4a87bc978745e833da76c72b2c373ca6b24e
Jason Armstrong <jasnarmstrng@gmail.com>
2022-11-13 13:20:18 +0000

    feat: Add min and max functions

--- pt-1-the-basics/chp-4-computation/exercises/src/min_max_mode_string.cpp
+++ pt-1-the-basics/chp-4-computation/exercises/src/min_max_mode_string.cpp
@@ -4,10 +4,22 @@ Definitions:
  min: shortest length string
  max: longest length string
  mode: string that appears most frequently
+
+ 1. Find min string: Store the first string and its length in an vector
+    of size 2. Compare the length of each subsequent string with the length in
+    the vector. If less then that string and length are the new values in
+    vector.
+ 2. Find max string: Same as 1 but if length is greater then we update the
+    vector with the new values.
+ 3. Find mode: Use the mode function previously created
 */
 
 #include <iostream>
 #include <vector>
+
+int min(int x, int y){ return (x>y) ? y : x; }
+int max(int x, int y){ return (x>y) ? x : y; }
+
 int main()
 {
 
