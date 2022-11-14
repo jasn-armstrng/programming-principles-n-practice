3af1cff5fcc1fd65a0636e5b39ef0c476bb61d57
Jason Armstrong <jasnarmstrng@gmail.com>
2022-11-14 18:19:20 +0000

    Create new file store_and_retrieve.cpp

---
+++ pt-1-the-basics/chp-4-computation/exercises/src/store_and_retrieve.cpp
@@ -0,0 +1,12 @@
+// This program stores and retrieves name/value pairs
+
+#include <iostream>
+#include <vector>
+int main()
+{
+  std::vector<std::string> names;
+  std::vector<int> scores;
+  std::string name;
+  int score;
+
+}
