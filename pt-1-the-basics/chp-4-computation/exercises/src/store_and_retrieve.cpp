1245c2ca205070b7332f95786b95be0f911c72b9
Jason Armstrong <jasnarmstrng@gmail.com>
2022-11-14 18:48:17 +0000

    feat: Add prompt for user input

--- pt-1-the-basics/chp-4-computation/exercises/src/store_and_retrieve.cpp
+++ pt-1-the-basics/chp-4-computation/exercises/src/store_and_retrieve.cpp
@@ -9,4 +9,31 @@ int main()
   std::string name;
   int score;
 
+  std::cout << "Please enter name and score. To end enter NoName 0.\n";
+  std::cout << "Name Score: ";
+  while(std::cin >> name >> score)
+  {
+    for(std::string n: names)
+    {
+      if (name == n)
+        std::cout << "Name already entered.\n";
+        break;
+    }
+
+    if(name == "NoName" && score == 0)
+    {
+      break;
+    }
+    else
+    {
+      names.push_back(name);
+      scores.push_back(score);
+    }
+  }
+  for (int i = 0; i < int(names.size()); ++i)
+  {
+    std::cout << "Name: " << names[i] << " Score: " << scores[i] << '\n';
+  }
+
+  return 0;
 }
