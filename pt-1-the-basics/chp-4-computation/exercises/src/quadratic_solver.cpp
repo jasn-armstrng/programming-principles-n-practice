88c34da28679ad8fd9ca3836fc77f9897c83891a
Jason Armstrong <jasnarmstrng@gmail.com>
2022-11-14 12:36:26 +0000

    feat: Add prompt for user input

--- pt-1-the-basics/chp-4-computation/exercises/src/quadratic_solver.cpp
+++ pt-1-the-basics/chp-4-computation/exercises/src/quadratic_solver.cpp
@@ -9,15 +9,28 @@ std::tuple<double, double> solve_quadratic(double a, double b, double c);
 
 int main()
 {
-  double result1, result2;
+  double a, b, c, result1, result2;
 
-  std::tie(result1, result2) = solve_quadratic(1, -5, 6);
-  std::cout << "\nEquation: x^2 + -5x + 6 = 0";
-  std::cout << "\nSolution: x=" << result1 << " or x=" << result2 << '\n';
+  std::cout << "+---------------------------+\n";
+  std::cout << "| Quadratic Equation Solver |\n";
+  std::cout << "+---------------------------+\n";
 
-  std::tie(result1, result2) = solve_quadratic(5, 6, 1);
-  std::cout << "\nEquation: 5x^2 + 6x + 1 = 0";
-  std::cout << "\nSolution: x=" << result1 << " or x=" << result2 << '\n';
+  std::cout << "\nEquation form: ax^2 + bx + c = 0\n\n";
+  std::cout << "a = "; std::cin >> a;
+  std::cout << "b = "; std::cin >> b;
+  std::cout << "c = "; std::cin >> c;
+
+  // std::tie(result1, result2) = solve_quadratic(1, -5, 6);
+  // std::cout << "\nEquation: x^2 + -5x + 6 = 0";
+  // std::cout << "\nSolution: x=" << result1 << " or x=" << result2 << '\n';
+  // Tests
+  // std::tie(result1, result2) = solve_quadratic(1, -5, 6);
+  // std::cout << "\nEquation: x^2 + -5x + 6 = 0";
+  // std::cout << "\nSolution: x=" << result1 << " or x=" << result2 << '\n'; // expect 3, 2
+
+  // std::tie(result1, result2) = solve_quadratic(5, 6, 1);
+  // std::cout << "\nEquation: 5x^2 + 6x + 1 = 0";
+  // std::cout << "\nSolution: x=" << result1 << " or x=" << result2 << '\n'; // expect -0.2, -1
 
   return 0;
 }
@@ -25,7 +38,7 @@ int main()
 std::tuple<double, double> solve_quadratic(double a, double b, double c)
 {
   double x1, x2;
-
+  // Compute solutions
   x1 = (-b + std::sqrt((b*b)-4*(a*c)))/(2*a);
   x2 = (-b - std::sqrt((b*b)-4*(a*c)))/(2*a);
 
