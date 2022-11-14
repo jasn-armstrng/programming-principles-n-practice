43bfb4c835a92673bdacd03cb41854399054db5a
Jason Armstrong <jasnarmstrng@gmail.com>
2022-11-14 19:05:16 +0000

    refactor: Switch while with for to read in
    user input.

--- pt-1-the-basics/chp-4-computation/exercises/src/store_and_retrieve.cpp
+++ pt-1-the-basics/chp-4-computation/exercises/src/store_and_retrieve.cpp
@@ -2,38 +2,51 @@
 
 #include <iostream>
 #include <vector>
+
+int mode(const std::vector<std::string> v);
+
 int main()
 {
   std::vector<std::string> names;
-  std::vector<int> scores;
-  std::string name;
-  int score;
+  std::vector<std::string> scores;
+  std::string name, score;
+
 
   std::cout << "Please enter name and score. To end enter NoName 0.\n";
   std::cout << "Name Score: ";
-  while(std::cin >> name >> score)
-  {
-    for(std::string n: names)
-    {
-      if (name == n)
-        std::cout << "Name already entered.\n";
-        break;
-    }
 
-    if(name == "NoName" && score == 0)
+  for (std::string name, score; std::cin >> name >> score;)
+  {
+    if(name == "NoName" && score == "0")
     {
       break;
     }
-    else
-    {
-      names.push_back(name);
-      scores.push_back(score);
-    }
   }
+
   for (int i = 0; i < int(names.size()); ++i)
   {
     std::cout << "Name: " << names[i] << " Score: " << scores[i] << '\n';
   }
 
   return 0;
+}
+
+int mode(const std::vector<std::string> v)
+{
+  int max_count = 0;
+
+  for(std::string i: v)
+  {
+    int count = 0;
+    for(std::string j: v)
+    {
+      if(i==j)
+        ++count;
+    }
+    if (count>max_count)
+    {
+      max_count = count;
+    }
+  }
+  return max_count;
 }
