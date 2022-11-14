4864bea41e8675b482cbf89f43cd5b7e29e0bc10
Jason Armstrong <jasnarmstrng@gmail.com>
2022-11-14 23:01:45 +0000

    feat: Add find_score function. Find players with a score

--- pt-1-the-basics/chp-4-computation/exercises/src/store_and_retrieve.cpp
+++ pt-1-the-basics/chp-4-computation/exercises/src/store_and_retrieve.cpp
@@ -6,13 +6,16 @@
 int mode(const std::vector<std::string> v);
 void show_all(const std::vector<std::string> n, const std::vector<std::string> s);
 void find_player_score(const std::vector<std::string> n, const std::vector<std::string> s);
+void find_score(const std::vector<std::string> n, const std::vector<std::string> s);
 
 int main()
 {
   std::vector<std::string> players;
   std::vector<std::string> scores;
   std::string player, score;
+  std::string queries = "\n[1] Show all scores\n[2] Find player's score\n[3] Find score\n[4] Exit\nOption: ";
   char option;
+
 
   std::cout << "Please enter player name and score on a new line below. To end enter: NoName 0\n";
   std::cout << "\nPlayer Score:\n";
@@ -36,23 +39,53 @@ int main()
     }
   }
 
-  std::cout << "\n[1] Show all scores\n[2] Find player score\n";
-  std::cout << "\nOption: "; std::cin >> option;
-  if (option == '1')
+  std::cout << queries;
+  while(std::cin >> option)
   {
-    show_all(players, scores);
-  }
-  if (option == '2')
-  {
-    find_player_score(players, scores);
+    if (option == '1')
+      show_all(players, scores);
+
+    if (option == '2')
+      find_player_score(players, scores);
+
+    if (option == '3')
+      find_score(players, scores);
+
+    if (option == '4')
+    {
+      std::cout << '\n';
+      break;
+    }
+
+    std::cout << queries;
   }
   return 0;
+}
+
+void find_score(const std::vector<std::string> n, const std::vector<std::string> s)
+{
+  std::string score;
+  int count = 0;
+  std::cout << "\nScore: "; std::cin >> score;
+
+  std::cout << "Players:\n";
+  std::cout << "--------\n";
+  for (int i = 0; i < int(s.size()); ++i)
+  {
+    if (s[i] == score)
+    {
+      ++count;
+      std::cout << n[i] << ", " << s[i] << '\n';
+    }
+  }
+  if (count == 0)
+    std::cout << "-, -\n";
 }
 
 void find_player_score(const std::vector<std::string> n, const std::vector<std::string> s)
 {
   std::string player, score = "-1";
-  std::cout << "Player: "; std::cin >> player;
+  std::cout << "\nPlayer: "; std::cin >> player;
   for (int i = 0; i < int(n.size()); ++i)
   {
     if (n[i] == player)
