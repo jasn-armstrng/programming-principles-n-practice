9a27d0e8cec03631dabb09847f9bcb4bbb7a7c27
Jason Armstrong <jasnarmstrng@gmail.com>
2022-11-16 22:14:16 +0000

    refactor: Handle exceptions thrown from ctok

--- pt-1-the-basics/chp-5-errors/exercises/src/celsius_to_kelvin.cpp
+++ pt-1-the-basics/chp-5-errors/exercises/src/celsius_to_kelvin.cpp
@@ -5,13 +5,25 @@ inline void keep_window_open();
 inline void error(const std::string& s);
 double ctok(double c);
 
-int main(){
+int main()
+try{
   double c = 0;
   std::cout << "Celsius: ";
   std::cin >> c;
-  std::cout << "Kelvin: " << ctok(c) << '\n';
+  double k = ctok(c);
+  std::cout << "Kelvin: " << k << '\n';
   keep_window_open();
   return 0;
+}
+catch(std::exception& e){
+  std::cerr << "\nError: " << e.what() << '\n';
+  keep_window_open();
+  return 1; // indicates failure
+}
+catch(...){
+  std::cerr << "\nError: Unknown exception" << '\n';
+  keep_window_open();
+  return 2; // indicates failure
 }
 
 inline void keep_window_open(){ char ch; std::cin>>ch; }
