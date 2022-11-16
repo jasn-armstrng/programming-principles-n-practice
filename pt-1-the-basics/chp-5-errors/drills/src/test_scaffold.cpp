b1711c176829eb5723516d72a68e8215772fdb46
Jason Armstrong <jasnarmstrng@gmail.com>
2022-11-16 21:07:31 +0000

    docs: Add description to the return types

--- pt-1-the-basics/chp-5-errors/drills/src/test_scaffold.cpp
+++ pt-1-the-basics/chp-5-errors/drills/src/test_scaffold.cpp
@@ -6,18 +6,19 @@ inline void keep_window_open();
 int main()
 try{
   // statement here
+  std::cout << "Success!\n";
   keep_window_open();
-  return 0; // indicates succes
+  return 0; // Indicates succes
 }
 catch (std::exception& e){
   std::cerr << "error: " << e.what() << '\n';
   keep_window_open();
-  return 1;
+  return 1; // Indicates failure
 }
 catch (...){
   std::cerr << "Oops: unknown exception!\n" << '\n';
   keep_window_open();
-  return 2;
+  return 2; // Indicates failure
 }
 
 inline void keep_window_open(){
