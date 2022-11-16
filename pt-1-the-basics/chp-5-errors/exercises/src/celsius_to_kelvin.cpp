c93d2f61fc813526786b63585a69fa9419e061ad
Jason Armstrong <jasnarmstrng@gmail.com>
2022-11-16 21:48:39 +0000

    refactor: Rename cel_to_kel as ctok
    The comments inside function will give details of purpose, ins and outs.
    Celsius or Kelvin will be found here in search of code base.

--- pt-1-the-basics/chp-5-errors/exercises/src/celsius_to_kelvin.cpp
+++ pt-1-the-basics/chp-5-errors/exercises/src/celsius_to_kelvin.cpp
@@ -1,18 +1,21 @@
 // This program converts Celsius to Kelvin
 #include <iostream>
-double cel_to_kel(double cel);
+double ctok(double c);
 inline void error(const std::string& s);
 int main(){
-
+  double c = 0;
+  std::cin >> c;
+  double k = ctok(c);
+  std::cout << k << " Kelvin" << '\n';
   return 0;
 }
 
-double cel_to_kel(double cel){
+double ctok(double c){
   // converts Celsius to Kelvin
   // pre-conditions: cel >= -273.15
   // post-conditions: returns a positive value. 0 Kelvin = absolute zero (-273.15c)
-  if (cel < -273.15) { error("Value entered is below absolute zero"); }
-  double kel = cel+273.15;
+  if (c < -273.15) { error("Value entered is below absolute zero"); }
+  double kel = c+273.15;
   return kel;
 }
 
