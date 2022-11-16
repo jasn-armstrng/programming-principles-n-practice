38d4e043e2c91c2634a97397c2c2597601a2c9de
Jason Armstrong <jasnarmstrng@gmail.com>
2022-11-16 22:01:53 +0000

    feat: Add keep_window_open function

--- pt-1-the-basics/chp-5-errors/exercises/src/celsius_to_kelvin.cpp
+++ pt-1-the-basics/chp-5-errors/exercises/src/celsius_to_kelvin.cpp
@@ -1,24 +1,27 @@
 // This program converts Celsius to Kelvin
 #include <iostream>
-double ctok(double c);
+
+inline void keep_window_open();
 inline void error(const std::string& s);
+double ctok(double c);
+
 int main(){
   double c = 0;
+  std::cout << "Celsius: ";
   std::cin >> c;
-  double k = ctok(c);
-  std::cout << k << " Kelvin" << '\n';
+  std::cout << "Kelvin: " << ctok(c) << '\n';
+  keep_window_open();
   return 0;
 }
+
+inline void keep_window_open(){ char ch; std::cin>>ch; }
+
+inline void error(const std::string& s){ throw std::runtime_error(s); }
 
 double ctok(double c){
   // converts Celsius to Kelvin
   // pre-conditions: cel >= -273.15
   // post-conditions: returns a positive value. 0 Kelvin = absolute zero (-273.15c)
   if (c < -273.15) { error("Value entered is below absolute zero"); }
-  double kel = c+273.15;
-  return kel;
-}
-
-inline void error(const std::string& s){
-	throw std::runtime_error(s);
+  return c+273.15;
 }
