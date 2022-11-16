03ebfd668b2432f1b0e651f6135561703a7bbe40
Jason Armstrong <jasnarmstrng@gmail.com>
2022-11-16 21:42:31 +0000

    feat: Add function cel_to_kel
    Checks if input is valid
    Converts Celsius to Kelvin

--- pt-1-the-basics/chp-5-errors/exercises/src/celsius_to_kelvin.cpp
+++ pt-1-the-basics/chp-5-errors/exercises/src/celsius_to_kelvin.cpp
@@ -2,23 +2,20 @@
 #include <iostream>
 double cel_to_kel(double cel);
 inline void error(const std::string& s);
-int main()
-{
+int main(){
+
   return 0;
 }
 
-double cel_to_kel(double cel)
-// converts Celsius to Kelvin
-// pre-conditions: cel >= -273.15
-// post-conditions: returns a positive value. 0 Kelvin = absolute zero (-273.15c)
-{
-  if (cel < -273.15)
-    std::error()
+double cel_to_kel(double cel){
+  // converts Celsius to Kelvin
+  // pre-conditions: cel >= -273.15
+  // post-conditions: returns a positive value. 0 Kelvin = absolute zero (-273.15c)
+  if (cel < -273.15) { error("Value entered is below absolute zero"); }
   double kel = cel+273.15;
   return kel;
 }
 
-inline void error(const std::string& s)
-{
+inline void error(const std::string& s){
 	throw std::runtime_error(s);
 }
