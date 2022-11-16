7e24d16f9156868274de8712f7f5ec3d8d2f6e81
Jason Armstrong <jasnarmstrng@gmail.com>
2022-11-16 21:32:26 +0000

    feat: Add function throw runtime errors
    Function is in the callee cel_to_kel

--- pt-1-the-basics/chp-5-errors/exercises/src/celsius_to_kelvin.cpp
+++ pt-1-the-basics/chp-5-errors/exercises/src/celsius_to_kelvin.cpp
@@ -1,6 +1,24 @@
 // This program converts Celsius to Kelvin
 #include <iostream>
+double cel_to_kel(double cel);
+inline void error(const std::string& s);
 int main()
 {
   return 0;
+}
+
+double cel_to_kel(double cel)
+// converts Celsius to Kelvin
+// pre-conditions: cel >= -273.15
+// post-conditions: returns a positive value. 0 Kelvin = absolute zero (-273.15c)
+{
+  if (cel < -273.15)
+    std::error()
+  double kel = cel+273.15;
+  return kel;
+}
+
+inline void error(const std::string& s)
+{
+	throw std::runtime_error(s);
 }
