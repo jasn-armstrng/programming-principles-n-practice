aeea633d2768361f85b4a10433f351f4fffe9bfa
Jason Armstrong <jasnarmstrng@gmail.com>
2022-11-17 20:22:57 +0000

    feat: Add function to present ui

--- pt-1-the-basics/chp-5-errors/exercises/src/solve_quadratic_eq.cpp
+++ pt-1-the-basics/chp-5-errors/exercises/src/solve_quadratic_eq.cpp
@@ -5,10 +5,14 @@
 inline void error(const std::string& s);
 inline void keep_window_open();
 void solve_quadratic(double a, double b, double c);
+void ui();
 
 int main()
 try{
-  solve_quadratic(1, 2, 3);
+  std::cout << "+--------------------------+\n";
+  std::cout << "| Solve Quadratic Equation |\n";
+  std::cout << "+--------------------------+\n\n";
+  ui();
   return 0;
 }
 catch(std::exception& e){
@@ -34,13 +38,21 @@ void solve_quadratic(double a, double b, double c){
   if(b*b-(4*a*c)<0){error("no real roots found");}
   double r1 = (-b + std::sqrt((b*b)-4*a*c))/(2*a);
   double r2 = (-b - std::sqrt((b*b)-4*a*c))/(2*a);
-  std::cout << "Root: " << r1 << " or " << r2 << '\n';
+  std::cout << "\nRoots: " << r1 << " and " << r2 << "\n";
 }
 
+void ui(){
+  // presents gui
+  // Note: Does not validate input.
+  double a, b, c;
+  std::cout << "For ax^2 + bx + c = 0, enter a b c: ";
+  std::cin>>a>>b>>c;
+  solve_quadratic(a, b, c);
+}
 
-// a = 1, b = -5, c = 6, Roots: x = 2, 3
-// a = 5, b = 6, c = 1, Roots: x = − 0.2, −1
-// a = 1, b = 2, c = 3, Roots: No real roots
-
-// solve_quadratic(1, -5, 6);
-// solve_quadratic(5, 6, 1);
+/*
+Test cases:
+a = 1, b = -5, c = 6, Roots: 2, 3
+a = 5, b = 6, c = 1, Roots: -0.2, -1
+a = 1, b = 2, c = 3, Roots: No real roots
+*/
