7b0b6d8c66d7b50ff882ac80d102406b4575b1f7
Jason Armstrong <jasnarmstrng@gmail.com>
2022-11-17 14:10:13 +0000

    feat: Add keep_window_open function

--- pt-1-the-basics/chp-5-errors/exercises/src/celsius_to_fahrenheit.cpp
+++ pt-1-the-basics/chp-5-errors/exercises/src/celsius_to_fahrenheit.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 
 inline void error(const std::string& s);
+inline void keep_window_open();
 double ctof(double c);
 double ftoc(double f);
 int main()
@@ -11,23 +12,30 @@ try{
   // std::cout << ctof(10) << '\n';
   // std::cout << ctof(-10) << '\n';
   // std::cout << ctof(100) << '\n';
-  // std::cout << ctof(-100) << '\n';
-  std::cout << ctof(-300) << '\n';
+  std::cout << ctof(-100) << '\n';
+  //std::cout << ctof(-300) << '\n';
+  keep_window_open();
   return 0;
 }
 catch(std::exception& e){
   std::cerr << "\nError: " << e.what() << '\n';
+  keep_window_open();
+  return 1;
 }
 catch(...){ // all other unthrown errors
   std::cerr << "\nError: unknown exception" << '\n';
+  keep_window_open();
+  return 2;
 }
 
 inline void error(const std::string& s){throw std::runtime_error(s);}
+
+inline void keep_window_open(){char ch; std::cin>>ch;};
 
 double ctof(double c){
   // converts celsius to fahrenheit
   // pre-conditions: c >= -273.15 (absolute zero)
   // post-conditions: returns value >= −459.67 F (absolute zero)
-  if (c < -273.15){ error("Value entered is below absolute zero");}
+  if(c < -273.15){ error("Value entered is below absolute zero");}
   return (c*9)/5+32;
 }
