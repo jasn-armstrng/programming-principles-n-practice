d53fc8830b0faf0f825f60c3b678ea2b4a61ebe9
Jason Armstrong <jasnarmstrng@gmail.com>
2022-11-17 13:59:16 +0000

    feat: Add function ctof, conv celsius->fahr

--- pt-1-the-basics/chp-5-errors/exercises/src/celsius_to_fahrenheit.cpp
+++ pt-1-the-basics/chp-5-errors/exercises/src/celsius_to_fahrenheit.cpp
@@ -1,7 +1,26 @@
 // This program does conversions between Celsius and Fahrenheit
 
 #include <iostream>
+
+inline void error(const std::string& s);
+double ctof(double c);
+double ftoc(double f);
 int main(){
 
+  std::cout << ctof(10) << '\n';
+  std::cout << ctof(-10) << '\n';
+  std::cout << ctof(100) << '\n';
+  std::cout << ctof(-100) << '\n';
+  //std::cout << ctof(-300) << '\n';
   return 0;
+}
+
+// inline void error(const std::string& s){throw std::runtime_error(s);}
+
+double ctof(double c){
+  // converts celsius to fahrenheit
+  // pre-conditions: c >= -273.15 (absolute zero)
+  // post-conditions: returns value >= −459.67 F (absolute zero)
+  //if (c < -273.15){ error("Value entered is below absolute zero");}
+  return (c*9)/5+32;
 }
