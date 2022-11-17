a72840c08b1f5550c5b94f9a0ead117553673267
Jason Armstrong <jasnarmstrng@gmail.com>
2022-11-17 20:06:55 +0000

    feat: Add keep_window_open function

--- pt-1-the-basics/chp-5-errors/exercises/src/solve_quadratic_eq.cpp
+++ pt-1-the-basics/chp-5-errors/exercises/src/solve_quadratic_eq.cpp
@@ -3,29 +3,29 @@
 #include <iostream>
 // function declaration(s)
 inline void error(const std::string& s);
+inline void keep_window_open();
 void solve_quadratic(double a, double b, double c);
+
 int main()
 try{
-  // a = 1, b = -5, c = 6, Roots: x = 2, 3
-  // a = 5, b = 6, c = 1, Roots: x = − 0.2, −1
-  // a = 1, b = 2, c = 3, Roots: No real roots
-
-  solve_quadratic(1, -5, 6);
-  solve_quadratic(5, 6, 1);
   solve_quadratic(1, 2, 3);
   return 0;
 }
 catch(std::exception& e){
   std::cerr << "\nError: " << e.what() << '\n';
+  keep_window_open();
   return 1;
 }
 catch(...){
   std::cerr << "\nError: Unknown exception\n";
+  keep_window_open();
   return 2;
 }
 
 // function definition(s)
 inline void error(const std::string& s){throw std::runtime_error(s);}
+
+inline void keep_window_open(){char ch; std::cin>>ch;}
 
 void solve_quadratic(double a, double b, double c){
   // compute the roots of a quadratic equation ax^2+bx+c=0
@@ -36,3 +36,11 @@ void solve_quadratic(double a, double b, double c){
   double r2 = (-b - std::sqrt((b*b)-4*a*c))/(2*a);
   std::cout << "Root: " << r1 << " or " << r2 << '\n';
 }
+
+
+// a = 1, b = -5, c = 6, Roots: x = 2, 3
+// a = 5, b = 6, c = 1, Roots: x = − 0.2, −1
+// a = 1, b = 2, c = 3, Roots: No real roots
+
+// solve_quadratic(1, -5, 6);
+// solve_quadratic(5, 6, 1);
