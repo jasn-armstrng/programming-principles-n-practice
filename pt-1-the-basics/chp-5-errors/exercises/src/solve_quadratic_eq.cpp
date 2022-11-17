6000d28e35dc2f0619fdf7021f6e7c1dba0c79ce
Jason Armstrong <jasnarmstrng@gmail.com>
2022-11-17 19:07:11 +0000

    feat: Add error function. Throw error

--- pt-1-the-basics/chp-5-errors/exercises/src/solve_quadratic_eq.cpp
+++ pt-1-the-basics/chp-5-errors/exercises/src/solve_quadratic_eq.cpp
@@ -2,6 +2,7 @@
 #include <cmath>
 #include <iostream>
 // function declaration(s)
+inline void error(const std::string& s);
 void solve_quadratic(double a, double b, double c);
 int main(){
   // a = 1, b = -5, c = 6, Roots: x = 2, 3
@@ -9,15 +10,19 @@ int main(){
 
   solve_quadratic(1, -5, 6);
   solve_quadratic(5, 6, 1);
+  solve_quadratic(1, 2, 3);
   return 0;
 }
 
 // function definition(s)
+inline void error(const std::string& s){throw std::runtime_error(s);}
+
 void solve_quadratic(double a, double b, double c){
   // compute the roots of a quadratic equation ax^2+bx+c=0
   // pre-conditions: b^2-4ac > 0
   // post-conditions: return the real roots of the quadratic equation
-  double r1 = (-b + std::sqrt((b*b)-4*(a*c)))/(2*a);
-  double r2 = (-b - std::sqrt((b*b)-4*(a*c)))/(2*a);
+  if(b*b-(4*a*c)<0){error("no real roots found");}
+  double r1 = (-b + std::sqrt((b*b)-4*a*c))/(2*a);
+  double r2 = (-b - std::sqrt((b*b)-4*a*c))/(2*a);
   std::cout << "Root: " << r1 << " or " << r2 << '\n';
 }
