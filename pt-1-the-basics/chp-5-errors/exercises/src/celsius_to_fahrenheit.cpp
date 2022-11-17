cc8a8f280f49b0590703f491de97d03f04606f88
Jason Armstrong <jasnarmstrng@gmail.com>
2022-11-17 14:48:34 +0000

    feat: Add user interface function, ui()

--- pt-1-the-basics/chp-5-errors/exercises/src/celsius_to_fahrenheit.cpp
+++ pt-1-the-basics/chp-5-errors/exercises/src/celsius_to_fahrenheit.cpp
@@ -1,19 +1,19 @@
 // This program does conversions between Celsius and Fahrenheit
-
 #include <iostream>
 
+// function declarations
 inline void error(const std::string& s);
 inline void keep_window_open();
 double ctof(double c);
 double ftoc(double f);
+void ui();
+
 int main()
 try{
-
-  std::cout << ftoc(10) << '\n';
-  std::cout << ftoc(-10) << '\n';
-  std::cout << ftoc(100) << '\n';
-  std::cout << ftoc(-100) << '\n';
-  //std::cout << ctof(-300) << '\n';
+  std::cout << "+------------------------+\n";
+  std::cout << "| Celsius <-> Fahrenheit |\n";
+  std::cout << "+------------------------+\n\n";
+  ui();
   keep_window_open();
   return 0;
 }
@@ -28,6 +28,7 @@ catch(...){ // all other unthrown errors
   return 2;
 }
 
+// functions definitions
 inline void error(const std::string& s){throw std::runtime_error(s);}
 
 inline void keep_window_open(){char ch; std::cin>>ch;};
@@ -35,8 +36,8 @@ inline void keep_window_open(){char ch; std::cin>>ch;};
 double ctof(double c){
   // converts celsius to fahrenheit
   // pre-conditions: c >= -273.15 (absolute zero)
-  // post-conditions: returns value >= -459.67 F (absolute zero)
-  if(c < -273.15){error("Value entered is below absolute zero");}
+  // post-conditions: returns value >= −459.67 F (absolute zero)
+  if(c < -273.15){error("Value entered is below absolute zero (-273.15 C)");}
   return (c*9)/5+32;
 }
 
@@ -44,6 +45,27 @@ double ftoc(double f){
   // converts fahrenheit to celsius
   // pre-conditions: f >= −459.67 F (absolute zero)
   // post-conditions: returns a celsius value > absolute 0 (-273.15)
-  if(f < -459.67){error("Value entered is below absolute zero");}
+  if(f < -459.67){error("Value entered is below absolute zero (-459.67 F)");}
   return (f-32)*5/9;
+}
+
+void ui(){
+  // presents gui
+  double v;
+  char u;
+
+  std::cout << "Enter temperature value and unit e.g. 32 c, 100 f\nTemp: ";
+  std::cin >>v>>u;
+
+  if(u=='c'){
+    double f = ctof(v);
+    std::cout << "Conv: " << f << " F\n";
+  }
+  else if(u=='f'){
+    double c = ftoc(v);
+    std::cout << "Conv: " << c << " ˚C\n";
+  }
+  else{
+    error("Bad input");
+  }
 }
