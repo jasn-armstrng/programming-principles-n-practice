26437c4905ef96beb7a94a66b919f25c3eb66f27
Jason Armstrong <jasnarmstrng@gmail.com>
2022-11-18 13:06:29 +0000

    feat: Throw error for invalid input into fibonacci

--- pt-1-the-basics/chp-5-errors/exercises/src/first_n_fibs.cpp
+++ pt-1-the-basics/chp-5-errors/exercises/src/first_n_fibs.cpp
@@ -7,21 +7,26 @@ inline void error(const std::string& s);
 void fibonacci(const int n);
 
 int main(){
-  fibonacci(20);
+  fibonacci(-1);
   return 0;
 }
 
 // function definitions
+inline void error(const std::string& s){throw std::runtime_error(s);}
 
 void fibonacci(const int n){
   // prints up to n numbers of the fibonacci series
   // pre-conditions: n must be a positive integer
   // post-conditions:
+  if(n<0){error("Invalid value for n");} // +ve integer check for n
+
   std::vector<int> series = {0, 1}; // initialize with first 2 fibs - f0, f1
+
   for(int i = 2; i<n; ++i){
     int fn = series[i-1]+series[i-2];
     series.push_back(fn);
   }
+
   for(int i = 0; i<int(series.size()); ++i){
     std::cout << i+1 << ". " << series[i] << '\n';
   }
