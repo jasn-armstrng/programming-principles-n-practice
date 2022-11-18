20b4538bb3e33a626d514a926231f75cff5096a3
Jason Armstrong <jasnarmstrng@gmail.com>
2022-11-18 04:46:16 +0000

    feat: Add prompt for user input

--- pt-1-the-basics/chp-5-errors/exercises/src/sum_of_first_n.cpp
+++ pt-1-the-basics/chp-5-errors/exercises/src/sum_of_first_n.cpp
@@ -1,6 +1,15 @@
 // This program computes the sum of the first n of a series of integers
 #include <iostream>
+#include <vector>
 int main(){
+  std::vector<int> integers;
+  int n;
 
+  std::cout << "Please enter the number of integers you want to sum: ";
+  std::cin>>n;
+  std::cout << "Please enter some integers (press '|' to stop): ";
+  for(int integer; std::cin>>integer;){integers.push_back(integer);}
+  std::cout << n << '\n';
+  for(int i: integers){std::cout << i << '\n';}
   return 0;
 }
