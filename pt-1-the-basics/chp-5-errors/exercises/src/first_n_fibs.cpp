f8da11baa0d764910cc6ecea87baa53df6067a5a
Jason Armstrong <jasnarmstrng@gmail.com>
2022-11-18 12:55:33 +0000

    feat: Add fibonacci function
    Generates the first n values of the fibonacci series

--- pt-1-the-basics/chp-5-errors/exercises/src/first_n_fibs.cpp
+++ pt-1-the-basics/chp-5-errors/exercises/src/first_n_fibs.cpp
@@ -1,6 +1,28 @@
 // This program prints the first n numbers of the fibonacci series
 #include <iostream>
-int main(){
+#include <vector>
 
+// function declarations
+inline void error(const std::string& s);
+void fibonacci(const int n);
+
+int main(){
+  fibonacci(20);
   return 0;
+}
+
+// function definitions
+
+void fibonacci(const int n){
+  // prints up to n numbers of the fibonacci series
+  // pre-conditions: n must be a positive integer
+  // post-conditions:
+  std::vector<int> series = {0, 1}; // initialize with first 2 fibs - f0, f1
+  for(int i = 2; i<n; ++i){
+    int fn = series[i-1]+series[i-2];
+    series.push_back(fn);
+  }
+  for(int i = 0; i<int(series.size()); ++i){
+    std::cout << i+1 << ". " << series[i] << '\n';
+  }
 }
