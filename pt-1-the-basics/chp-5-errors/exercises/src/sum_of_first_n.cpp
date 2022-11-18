66a45936b305cf61cd0b2da65de0ea08fb1d856e
Jason Armstrong <jasnarmstrng@gmail.com>
2022-11-18 05:39:45 +0000

    feat: Add error function
    Throws error when n > vector.size().

--- pt-1-the-basics/chp-5-errors/exercises/src/sum_of_first_n.cpp
+++ pt-1-the-basics/chp-5-errors/exercises/src/sum_of_first_n.cpp
@@ -1,15 +1,43 @@
 // This program computes the sum of the first n of a series of integers
+/*
+Program execution:
+1. Ask for n - the number of integers in series to sum. Read into n.
+2. Ask for series of integers to sum. Read into vector.
+3. Before computing sum check if n <= vector.size().
+4. If 3 is false,
+    - Show error
+    - Repeat the prompt for n and integers
+5. If 3 is true compute the sum of the first n integers in vector.
+6. Output n, sum and integers that make up sum.
+7. End program.
+*/
 #include <iostream>
 #include <vector>
+
+// function declarations
+inline void error(const std::string& s);
+
 int main(){
   std::vector<int> integers;
-  int n;
+  int n, sum = 0;
 
-  std::cout << "Please enter the number of integers you want to sum: ";
+  std::cout << "Please enter the number of integers you want to sum:\n";
   std::cin>>n;
-  std::cout << "Please enter some integers (press '|' to stop): ";
+  std::cout << "Please enter some integers (press '|' to stop):\n";
   for(int integer; std::cin>>integer;){integers.push_back(integer);}
-  std::cout << n << '\n';
-  for(int i: integers){std::cout << i << '\n';}
+  // n, vector length comparison
+  if(!(n<=int(integers.size()))){error("Not enough integers provided");}
+  for(int i = 0; i<n; ++i){sum+=integers[i];}
+  std::cout << "The sum of the first " << n << " numbers is " << sum << '\n';
+
   return 0;
 }
+
+// function definitions
+inline void error(const std::string& s){throw std::runtime_error(s);}
+
+
+
+// Test code:
+// std::cout << n << '\n';
+// for(int i: integers){std::cout << i << '\n';}
