3a07eedfb02d9bdea50df55fd8943b57600a84d5
Jason Armstrong <jasnarmstrng@gmail.com>
2022-11-19 14:49:29 +0000

    feat: Add random function generators
    These function generators are taken from std_lib_facilities.h

--- pt-1-the-basics/chp-5-errors/exercises/src/bulls_and_cows.cpp
+++ pt-1-the-basics/chp-5-errors/exercises/src/bulls_and_cows.cpp
@@ -1,8 +1,8 @@
 /*
 This program implements the guessing game 'Bulls and Cows'.
-A sequence of 4 integers in the range 0-9 are selected. The integers do not
-repeat. Through guesses and hints a user has to discover the 4 in the correct
-order.
+A sequence of n integers in the range 0-9 are selected. The integers do not
+repeat. Through guesses and hints a user has to discover the integers in the
+correct order.
 
 How the guess and hints work:
 - If the number is 1234 and the guess is 1359 the hint should be '1 bull, 1 cow'
@@ -12,8 +12,35 @@ How the guess and hints work:
   bulls.
 */
 #include <iostream>
+#include <random>
 #include <vector>
+
+// ------------------------- function declarations -----------------------------
+std::vector<int> random_integers(const int n);
+inline int randint(int min, int max);
+inline int randint(int max);
+
 int main(){
+
 
   return 0;
 }
+
+// ------------------------- function definitions ------------------------------
+std::vector<int> random_integers(const int n){
+  // generate n random (non-repeating) integers in range 0-9
+  // pre-conditions: n>1
+  // post-conditions: return a vector of random integers
+  std::vector<int> random_ints;
+  return random_ints;
+}
+
+// random number generators taken from std_lib_facilities.h
+inline std::default_random_engine& get_rand(){
+	static std::default_random_engine ran;	// note: not thread_local
+	return ran;
+};
+
+inline int randint(int min, int max){return std::uniform_int_distribution<>{min, max}(get_rand());}
+
+inline int randint(int max){return randint(0, max);}
