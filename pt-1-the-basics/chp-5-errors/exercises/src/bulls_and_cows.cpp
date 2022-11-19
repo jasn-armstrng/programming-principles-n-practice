38e89587eaa6db6c492f6c77ede9b46ca43cd4ec
Jason Armstrong <jasnarmstrng@gmail.com>
2022-11-19 14:06:03 +0000

    Create new file bulls_and_cows.cpp

---
+++ pt-1-the-basics/chp-5-errors/exercises/src/bulls_and_cows.cpp
@@ -0,0 +1,19 @@
+/*
+This program implements the guessing game 'Bulls and Cows'.
+A sequence of 4 integers in the range 0-9 are selected. The integers do not
+repeat. Through guesses and hints a user has to discover the 4 in the correct
+order.
+
+How the guess and hints work:
+- If the number is 1234 and the guess is 1359 the hint should be '1 bull, 1 cow'
+  because the got one digit (1) right and in the right position - the bull. The
+  user also got another digit (3) but in the wrong position - the cow.
+- Guessing/hints continue until the user gets all 4 numbers in right orider - 4
+  bulls.
+*/
+#include <iostream>
+#include <vector>
+int main(){
+
+  return 0;
+}
