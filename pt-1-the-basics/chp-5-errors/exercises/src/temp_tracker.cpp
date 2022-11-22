c813c662c2d016c20f24c143c2bfecc8155771d2
Jason Armstrong <jasnarmstrng@gmail.com>
2022-11-22 11:41:02 +0000

    Create new file temp_tracker.cpp

---
+++ pt-1-the-basics/chp-5-errors/exercises/src/temp_tracker.cpp
@@ -0,0 +1,9 @@
+// This program stores and presents the avg. temperature of all the days of a
+// given week, computes and presents the avg. temperature for the given week.
+
+#include <iostream>
+#include <vector>
+int main(){
+
+  return 0;
+}
