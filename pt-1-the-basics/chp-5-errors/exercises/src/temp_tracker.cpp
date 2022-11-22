f9ac5fcb7cb5f59494436b653d49c36022d1c7d4
Jason Armstrong <jasnarmstrng@gmail.com>
2022-11-22 13:07:40 +0000

    Add prompt for User input

--- pt-1-the-basics/chp-5-errors/exercises/src/temp_tracker.cpp
+++ pt-1-the-basics/chp-5-errors/exercises/src/temp_tracker.cpp
@@ -1,9 +1,23 @@
 // This program stores and presents the avg. temperature of all the days of a
 // given week, computes and presents the avg. temperature for the given week.
-
 #include <iostream>
 #include <vector>
 int main(){
+  int weeks = 52, days = 7;
+  std::vector<std::vector<int>> temperatures; // temperatures is a 2D vector
+  temperatures.resize(weeks, std::vector<int>(days, 0)); // initialize as 52*7
+
+  int week_number; // corresponds to a lvl-1 index of the temp vector
+  std::string day; // mapped to a lvl-2 index of the temp vector
+  int temp; // value at a lvl-2 index of the temp vector
+
+  // get data from user
+  std::cout << "Enter the week#: ";
+  std::cin>>week_number;
+  std::cout << "Enter the day: ";
+  std::cin>>day;
+  std::cout << "Enter the avg. temperature: ";
+  std::cin>>temp;
 
   return 0;
 }
