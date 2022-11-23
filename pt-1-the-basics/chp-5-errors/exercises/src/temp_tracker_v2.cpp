af70c4cc3eeab0aab648c57ebefa312f82ee7b37
Jason Armstrong <jasnarmstrng@gmail.com>
2022-11-23 17:48:23 +0000

    Create new file temp_tracker_v2.cpp

---
+++ pt-1-the-basics/chp-5-errors/exercises/src/temp_tracker_v2.cpp
@@ -0,0 +1,9 @@
+// This program collects day/temp values from user in a vector.
+// It rejects invalid days of the week and writes out at the end
+// the temps stored and the number of rejected values
+#include <iostream>
+#include <vector>
+
+int main() {
+
+}
