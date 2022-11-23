53a0d51656a2dcb8304ca1fb6a9b1ebeff3b7a5c
Jason Armstrong <jasnarmstrng@gmail.com>
2022-11-23 21:33:50 +0000

    refactor: Move UI elements to function

--- pt-1-the-basics/chp-5-errors/exercises/src/temp_tracker_v2.cpp
+++ pt-1-the-basics/chp-5-errors/exercises/src/temp_tracker_v2.cpp
@@ -1,22 +1,57 @@
-// This program collects day/temp values from user in a vector.
-// It rejects invalid days of the week and writes out at the end
-// the temps stored and the number of rejected values
+/*
+  This program collects day/temp values from user and stores them in a vector.
+  It rejects invalid days of the week and writes out at the end the temps stored
+  and the number of rejected values.
+
+  Sample output,
+  Day:    Mon     Tues    Thur    Sat
+  Temp:   23      24      23      24
+
+  Rejected entries: 4
+*/
 #include <iostream>
 #include <vector>
 
 bool valid_day(const std::string s);
+void ui();
 
 int main() {
+  std::cout << "+---------------------+\n";
+  std::cout << "| Temperature Tracker |\n";
+  std::cout << "+---------------------+\n\n";
+  ui();
+  return 0;
+}
+
+bool valid_day(std::string d) {
+  // validate day input
+  // pre-condition: d must be a string
+  // post-condtion: return validation as a boolean value
+  std::vector<std::string> valid_days = { "Monday", "Mon", "monday",
+  "Tuesday", "Tues", "tuesday", "Wednesday", "Wed", "wednesday", "Thursday",
+  "Thur", "thursday", "Friday", "Fri", "friday", "Saturday", "Sat", "Saturday",
+  "Sunday", "Sun", "sunday" };
+
+  for (std::string s: valid_days) { if (d == s) { return true; } }
+  return false;
+}
+
+void ui() {
   std::vector<std::string> days;
   std::vector<int> temps;
-
   std::string day;
-  int temp;
+  int temp, rejected_entries = 0;
 
-  int rejected_entries = 0;
+  std::string instructions =
+  "How to use this program:\n"
+  "- Enter day/temperature pairs e.g. Mon 23 Tuesday 24 ...\n"
+  "- A valid day can take the forms Monday, monday, or Mon.\n"
+  "- Invalid days will be rejected and total rejected displayed at the end.\n";
+
+  std::cout << instructions;
 
   // prompt user input
-  std::cout << "Enter day/temp pairs e.g. Tues 23 (press ctrl+D to stop)\nPairs: ";
+  std::cout << "\nPlease enter day/temp pairs (press ctrl+D to stop)\nPairs: ";
   while (std::cin >> day >> temp) {
     // input validation for day
     if(!valid_day(day)) {
@@ -28,27 +63,15 @@ int main() {
     }
   }
 
+  // output days, temps and no. of rejected entries to user
   std::cout << "\nDay:\t";
   for (std::string d: days) { std::cout << d << '\t'; }
   std::cout << "\nTemp:\t";
   for (int t: temps) { std::cout << t << '\t'; }
-  std::cout << '\n';
-  std::cout << "\nRejected entries: " << rejected_entries << '\n';
 
-  return 0;
+  std::cout << "\n\nRejected entries: " << rejected_entries << '\n';
 }
 
-bool valid_day(std::string d) {
-   // use to validate day input
-  std::vector<std::string> valid_days = { "Monday", "Mon", "monday",
-  "Tuesday", "Tues", "tuesday", "Wednesday", "Wed", "wednesday", "Thursday",
-  "Thur", "thursday", "Friday", "Fri", "friday", "Saturday", "Sat", "Saturday",
-  "Sunday", "Sun", "sunday" };
-
-  for (std::string s: valid_days) {
-    if (d == s) {
-      return true;
-    }
-  }
-  return false;
-}
+// Test stream
+// Mon 23 Tues 24 Thur 23 Funday 23 Sat 24 Subway 23 Monday 22 Chuesday 21
+// Should reject 3
