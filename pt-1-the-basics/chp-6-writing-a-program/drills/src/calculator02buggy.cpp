8c26f2bc033bf703515de6ace5e44accb501b90a
Jason Armstrong <jasnarmstrng@gmail.com>
2022-11-29 13:17:50 +0000

    refactor: Change print resilt command from ; to =

--- pt-1-the-basics/chp-6-writing-a-program/drills/src/calculator02buggy.cpp
+++ pt-1-the-basics/chp-6-writing-a-program/drills/src/calculator02buggy.cpp
@@ -88,11 +88,12 @@ Token Token_stream::get()
     cin >> ch;    // note that >> skips whitespace (space, newline, tab, etc.)
 
     switch (ch) {
-        case ';':    // for "print"
+        case '=':    // for "print"
         case 'q':    // for "quit"
         case '(': case ')': case '+': case '-': case '*': case '/':
             return Token(ch);        // let each character represent itself.
-                                     // return Token(ch) for all above character input cases
+                                     // return Token(ch) for all above character
+                                     // input cases
         case '.':
         case '0': case '1': case '2': case '3': case '4':
         case '5': case '6': case '7': case '8': case '9':
@@ -205,8 +206,8 @@ try
         Token t = ts.get(); // inside get() is the cin>>variable
 
         if (t.kind == 'q') break; // 'q' for quit
-        if (t.kind == ';')        // ';' for "print now"
-            cout << "=" << val << '\n';
+        if (t.kind == '=')        // ';' for "print now"
+            cout << val << '\n';
         else
             ts.putback(t);
         val = expression();
