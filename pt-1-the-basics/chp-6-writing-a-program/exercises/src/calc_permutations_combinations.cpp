0661e56806c71396cbbaf06f7b348e4732ff1f83
Jason Armstrong <jasnarmstrng@gmail.com>
2022-12-22 12:59:01 +0000

    Create new project calc_permutations_combinations.cpp

---
+++ pt-1-the-basics/chp-6-writing-a-program/exercises/src/calc_permutations_combinations.cpp
@@ -0,0 +1,26 @@
+/*
+  A permutation is an ordered subset of a set. For example, say you wanted to
+  pick a combination to a vault. There are 60 possible numbers, and you need
+  three different numbers for the combination. There are P(60, 3) permutations
+  for the combination, where P is defined by the formula,
+              P(a, b) = a!/(a-b)!
+
+  Combinations are similar to permutations except the order of the of the objects
+  does not matter. The formula for combinations is,
+              C(a, b) = P(a, b)/b!
+
+  Program requirements:
+  - Ask user for two integers
+  - Ask if to calculate Permutations or Combinations
+  - Print the result
+  - Check for input errors
+  - All erroneous inputs will generate good error messages
+
+  Analysis:
+
+  Design:
+
+  Implementation:
+
+  Testing:
+*/
