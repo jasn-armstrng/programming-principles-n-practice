aa9c8fce9322da81b5ff1ba435469f6cd13d512a
Jason Armstrong <jasnarmstrng@gmail.com>
2023-01-11 23:31:17 -0400

    Define namespaces X, Y, Z

--- pt-1-the-basics/chp-8-technicalities-functions-etc/drills/drill_3/src/main.cpp
+++ pt-1-the-basics/chp-8-technicalities-functions-etc/drills/drill_3/src/main.cpp
@@ -1,18 +1,33 @@
 #include <iostream>
 
+namespace X {
+  int var;
+  void print() { std::cout << var << '\n'; }
+}
+
+namespace Y {
+  int var;
+  void print() { std::cout << var << '\n'; }
+}
+
+namespace Z {
+  int var;
+  void print() { std::cout << var << '\n'; }
+}
+
 int main() {
   X::var = 7;
   X::print();         // print X's var
   using namespace Y;
-  var = 9;
+  var = 9;            // This is Y's var
   print();            // print Y's var
   {
     using Z::var;
-    using Z::print;   // print Z's var
+    using Z::print;
     var = 11;
-    print();
+    print();          // print Z's var. We're in a Z exclusive block
   }
-  print();            // print Y's var
+  print();            // print Y's var, because "using namespace Y" stated explicitly
   X::print();         // print X's var
   return 0;
 }
