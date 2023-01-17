58f1cbd106d1be1508bf6250fd21641ec345cff6
Jason Armstrong <jasnarmstrng@gmail.com>
2023-01-17 00:30:00 -0400

    refactor: Add header file
    Move declarations for the referenced vector<int> v and the fibonacci()
    and print functions() to header.

---
+++ pt-1-the-basics/chp-8-technicalities-functions-etc/exercises/print_fibonacci/headers/print_fibonacci.h
@@ -0,0 +1,5 @@
+#include <vector>
+
+extern std::vector<int> v;
+std::vector<int> fibonacci(const int x, const int y, std::vector<int>& v, const int n);
+void print(const std::string label, const std::vector<int>& v);
