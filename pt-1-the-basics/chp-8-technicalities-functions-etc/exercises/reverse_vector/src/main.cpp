e1086c4d2e25d19f9f457122be873781938ecf0b
Jason Armstrong <jasnarmstrng@gmail.com>
2023-01-17 12:40:54 -0400

    feat: Add reverse_by_reference()
    Reverses vector contents in place. Returns modified vector.
    Add print()

--- pt-1-the-basics/chp-8-technicalities-functions-etc/exercises/reverse_vector/src/main.cpp
+++ pt-1-the-basics/chp-8-technicalities-functions-etc/exercises/reverse_vector/src/main.cpp
@@ -2,11 +2,14 @@
 #include <vector>
 
 std::vector<int> reverse_by_value(const std::vector<int> v);
+std::vector<int> reverse_by_reference(std::vector<int>& v);
 void print(const std::vector<int> v);
 
 int main() {
   std::vector<int> odd_numbers = {1, 3, 5, 7, 9};
+  print(odd_numbers);
   print(reverse_by_value(odd_numbers));
+  print(reverse_by_reference(odd_numbers));
   return 0;
 }
 
@@ -21,9 +24,24 @@ std::vector<int> reverse_by_value(const std::vector<int> v) {
   return v_reversed;
 }
 
+std::vector<int> reverse_by_reference(std::vector<int>& v) {
+  // reverse contents of a vector
+  // pre-conditions: vector v is not empty
+  // post-conditions: returns v reversed
+  for (unsigned i = v.size(), j = 0; i > v.size()/2; --i, ++j) {
+    // swap(last, first), swap(2nd-to-last, 2nd), ... till the middle
+    std::swap(v[i-1], v[j]);
+  }
+  return v;
+}
+
 void print(const std::vector<int> v) {
   // print the contents of a vector
-  for(int e: v) {
-    std::cout << e << '\n';
+  for(unsigned i = 0; i < v.size(); ++i) {
+    if(i != 0) {
+      std::cout << ", ";
+    }
+    std::cout << v[i];
   }
+  std::cout << '\n';
 }
