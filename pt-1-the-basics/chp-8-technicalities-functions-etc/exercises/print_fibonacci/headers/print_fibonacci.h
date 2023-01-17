0d948949015021585945b355ace2e55f24dc4e78
Jason Armstrong <jasnarmstrng@gmail.com>
2023-01-17 10:42:05 -0400

    feat: Add simple GUI

--- pt-1-the-basics/chp-8-technicalities-functions-etc/exercises/print_fibonacci/headers/print_fibonacci.h
+++ pt-1-the-basics/chp-8-technicalities-functions-etc/exercises/print_fibonacci/headers/print_fibonacci.h
@@ -3,3 +3,4 @@
 extern std::vector<int> v;
 std::vector<int> fibonacci(const int x, const int y, std::vector<int>& v, const int n);
 void print(const std::string label, const std::vector<int>& v);
+void gui();
