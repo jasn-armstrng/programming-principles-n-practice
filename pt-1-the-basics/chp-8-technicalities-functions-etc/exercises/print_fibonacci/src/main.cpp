e11b1c785a5faeab296174c611b33dc073573402
Jason Armstrong <jasnarmstrng@gmail.com>
2023-01-17 00:54:58 -0400

    docs: Move program description to main

--- pt-1-the-basics/chp-8-technicalities-functions-etc/exercises/print_fibonacci/src/main.cpp
+++ pt-1-the-basics/chp-8-technicalities-functions-etc/exercises/print_fibonacci/src/main.cpp
@@ -1,8 +1,10 @@
+// This program computes and prints a custom Fibonacci sequence; the first 2
+// elements and the length of the sequence are controlled.
 #include <iostream>
 #include "../headers/print_fibonacci.h"
 
 int main() {
-  fibonacci(0, 1, v, 10);
-  print("First 10 Fibonacci numbers", v);
+  fibonacci(3, 5, v, 10);
+  print("Fibonacci Sequence", v);
   return 0;
 }
--- pt-1-the-basics/chp-8-technicalities-functions-etc/exercises/print_fibonacci/src/print_fibonacci.cpp
+++ pt-1-the-basics/chp-8-technicalities-functions-etc/exercises/print_fibonacci/src/print_fibonacci.cpp
@@ -1,5 +1,3 @@
-// This program computes and prints a custom Fibonacci sequence; the first 2
-// elements and the length of the sequence are controlled.
 #include <iostream>
 #include "../headers/print_fibonacci.h"
 
