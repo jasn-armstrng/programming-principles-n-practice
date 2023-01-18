f4878ac201cc46f2a2e606cf8bf4c962507952af
Jason Armstrong <jasnarmstrng@gmail.com>
2023-01-18 16:17:49 -0400

    Create new project compute_index

---
+++ pt-1-the-basics/chp-8-technicalities-functions-etc/exercises/compute_index/src/compute_index.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include "../headers/compute_index.h"
+
+inline void error(const std::string& s) { throw std::runtime_error(s); }
+
+double compute_index(const std::vector<double> x, const std::vector<double> y) {
+  // computes index: sum of all x[i]*y[i]
+  // pre-conditions: vector x and y must be of same size and size > 0
+  // post-conditions: return sum
+  if(x.size() != y.size()) {
+    error("Inputs are not the same size!");
+  }
+
+  if(x.size() == 0 || y.size() == 0) {
+    error("Input contains no data!");
+  }
+
+  double sum {0};
+  for(unsigned i = 0; i < x.size(); ++i) {
+    sum += x[i]*y[i];
+  }
+  return sum;
+}
---
+++ pt-1-the-basics/chp-8-technicalities-functions-etc/exercises/compute_index/src/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include "../headers/compute_index.h"
+
+int main() try {
+  std::vector<double> price = {2.5, 3.5};
+  std::vector<double> weight = {1, 2};
+  std::cout << "Index: " << compute_index(price, weight) << '\n';
+  return 0;
+}
+catch(std::exception& e) {
+  std::cerr << "Error: " << e.what() << '\n';
+  return 1;
+}
+catch(...) {
+  std::cerr << "Error: Unknown exception" << '\n';
+  return 2;
+}
