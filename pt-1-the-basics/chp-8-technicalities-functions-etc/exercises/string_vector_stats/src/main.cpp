3853fb3521d2ed7518f967a8211068978bcf5175
Jason Armstrong <jasnarmstrng@gmail.com>
2023-01-18 21:47:16 -0400

    Create new project string_vector_stats

---
+++ pt-1-the-basics/chp-8-technicalities-functions-etc/exercises/string_vector_stats/src/main.cpp
@@ -0,0 +1,12 @@
+/*
+Given a vector of strings, this program conputes and returns,
+1. The number of cheracters in each string
+2. The longest and shortest strings
+3. The lexicographical shortest and longest string
+*/
+#include <iostream>
+#include <vector>
+
+int main() {
+  return 0;
+}
