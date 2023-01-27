152d6cd58a85a69724d72f48a13c1242dcd51a02
Jason Armstrong <jasnarmstrng@gmail.com>
2023-01-26 20:11:30 -0400

    refactor: Create class method get_info()
    Move object data console out to get_info()

--- pt-1-the-basics/chp-9-technicalities-classes-etc/examples/youtube-channel/src/youtube_channel.cpp
+++ pt-1-the-basics/chp-9-technicalities-classes-etc/examples/youtube-channel/src/youtube_channel.cpp
@@ -15,6 +15,17 @@ class YouTubeChannel {
       owner_name = owner_name_;
       subscriber_count = 0;
     }
+
+    void get_info() {
+      // print attributes to console
+      std::cout << "Name: " << name << '\n';
+      std::cout << "Owner name: " << owner_name << '\n';
+      std::cout << "Subscriber count: " << subscriber_count << '\n';
+      std::cout << "Videos:\n";
+      for(std::string video: published_video_titles) {
+        std::cout << " - " << video << '\n';
+      }
+    }
 };
 
 int main() {
@@ -24,27 +35,14 @@ int main() {
   yt_channel.published_video_titles.push_back("HTML & CSS Video 1");
   yt_channel.published_video_titles.push_back("C++ OOP Video 1");
 
-  // print attributes to console
-  std::cout << "Name: " << yt_channel.name << '\n';
-  std::cout << "Owner name: " << yt_channel.owner_name << '\n';
-  std::cout << "Subscriber count: " << yt_channel.subscriber_count << '\n';
-  std::cout << "Videos:\n";
-  for(std::string video: yt_channel.published_video_titles) {
-    std::cout << " - " << video << '\n';
-  }
+  yt_channel.get_info();
 
   // channel instance 2
   YouTubeChannel yt_channel2("AmySings", "Amy");
-  yt_channel.published_video_titles.push_back("Johnny B - Cover");
-  yt_channel.published_video_titles.push_back("Lorelei - Cover");
+  yt_channel2.published_video_titles.push_back("Johnny B - Cover");
+  yt_channel2.published_video_titles.push_back("Lorelei - Cover");
 
-  std::cout << "Name: " << yt_channel2.name << '\n';
-  std::cout << "Owner name: " << yt_channel2.owner_name << '\n';
-  std::cout << "Subscriber count: " << yt_channel2.subscriber_count << '\n';
-  std::cout << "Videos:\n";
-  for(std::string video: yt_channel2.published_video_titles) {
-    std::cout << " - " << video << '\n';
-  }
+  yt_channel2.get_info();
 
   return 0;
 }
