82f2d8b48db8bdb9efafebfcdc334fa05395a740
Jason Armstrong <jasnarmstrng@gmail.com>
2023-01-28 00:05:15 -0400

    feat: Add like() to base class

--- pt-1-the-basics/chp-9-technicalities-classes-etc/examples/youtube-channel/src/youtube_channel.cpp
+++ pt-1-the-basics/chp-9-technicalities-classes-etc/examples/youtube-channel/src/youtube_channel.cpp
@@ -5,11 +5,12 @@ class YouTubeChannel {
   public:
   // interface
 
-    // YouTubeChannel class object constructor
+    // YouTubeChannel constructor
     YouTubeChannel(std::string name_, std::string owner_name_) {
       name = name_;
       owner_name = owner_name_;
       subscriber_count = 0;
+      likes = 0;
     }
 
     void get_info() {
@@ -17,6 +18,7 @@ class YouTubeChannel {
       std::cout << "Name: " << name << '\n';
       std::cout << "Owner name: " << owner_name << '\n';
       std::cout << "Subscriber count: " << subscriber_count << '\n';
+      std::cout << "Likes: " << likes << '\n';
       std::cout << "Videos:\n";
       for(std::string video: published_video_titles) {
         std::cout << " - " << video << '\n';
@@ -38,6 +40,11 @@ class YouTubeChannel {
       published_video_titles.push_back(title);
     }
 
+    void like(){
+      ++likes;
+    }
+
+    // setters
     void set_name(const std::string& new_name) {
       name = new_name;
     }
@@ -50,6 +57,7 @@ class YouTubeChannel {
       about = new_about;
     }
 
+    // getters
     void get_name() {
       std::cout << name << '\n';
     }
@@ -59,21 +67,30 @@ class YouTubeChannel {
     }
 
     void get_about() {
-      std::cout << name << ":" << about << '\n';
+      std::cout << name << ": "<< about << '\n';
+    }
+
+    void check_analytics() {
+      if(likes<5) {
+        std::cout << "This channel has low rated content\n";
+      } else {
+        std::cout << "This channel has high rated content\n";
+      }
     }
 
   private:
   // implementation details
     // encapsulation: These properties should be updated using a methods via
     // the class' public interface
+    std::string name;
     std::string owner_name;
-    int subscriber_count;
+    std::string about;
     std::list<std::string> published_video_titles;
+    int subscriber_count;
+    int likes;
 
   protected:
-    // make member accessible to derived classes
-    std::string name;
-    std::string about;
+    // members here are accessible to all derived classes
 };
 
 // here CookingYouTubeChannel (a derived class) inherits everything from
@@ -81,6 +98,15 @@ class YouTubeChannel {
 class CookingYouTubeChannel:public YouTubeChannel {
   public:                                                            // inherit the constructor from base
     CookingYouTubeChannel(std::string name_, std::string owner_name_):YouTubeChannel(name_, owner_name_) {
+      // ...
+    }
+};
+
+// here CookingYouTubeChannel (a derived class) inherits everything from
+// YouTubeChannel (the base class)
+class SingingYouTubeChannel:public YouTubeChannel {
+  public:                                                            // inherit the constructor from base
+    SingingYouTubeChannel(std::string name_, std::string owner_name_):YouTubeChannel(name_, owner_name_) {
       // ...
     }
 };
@@ -126,11 +152,41 @@ int main() {
   cyt_channel.publish_video("Apple Pie");
   cyt_channel.publish_video("Chocolate cake");
 
+  cyt_channel.like();
+  cyt_channel.like();
+  cyt_channel.like();
+  cyt_channel.like();
+  cyt_channel.like();
+  cyt_channel.like();
+
   cyt_channel.subscribe();
   cyt_channel.subscribe();
 
   cyt_channel.get_info();
   cyt_channel.get_about(); // ideally this should be inherited from the base
+
+  std::cout << "\n[Singing Channel]\n";
+  // new singing channel
+  CookingYouTubeChannel syt_channel("John Sings", "John");
+  syt_channel.set_about("Taking singing classes, learning new songs, learning how to dance ...");
+  syt_channel.publish_video("Esc Sequence - Cover");
+  syt_channel.like();
+
+  syt_channel.subscribe();
+  syt_channel.subscribe();
+  syt_channel.subscribe();
+
+  syt_channel.get_info();
+  syt_channel.get_about(); // ideally this should be inherited from the base
+
+
+  YouTubeChannel * yt1 {&cyt_channel};
+  YouTubeChannel * yt2 {&syt_channel};
+
+  std::cout << "\n[Channel Analytics]\n";
+
+  yt1->check_analytics();
+  yt2->check_analytics();
 
   return 0;
 }
