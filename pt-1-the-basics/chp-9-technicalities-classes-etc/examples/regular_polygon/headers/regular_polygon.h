e39bd426c3ac5fba5bc1b4e6f4ded202854e1c42
Jason Armstrong <jasnarmstrng@gmail.com>
2023-01-28 21:58:37 -0400

    feat: Throw exception from constructor
    when either width or height is less than 1.
    Catch exceptions in main()

--- pt-1-the-basics/chp-9-technicalities-classes-etc/examples/regular_polygon/headers/regular_polygon.h
+++ pt-1-the-basics/chp-9-technicalities-classes-etc/examples/regular_polygon/headers/regular_polygon.h
@@ -5,7 +5,14 @@ namespace Shape {
   class RegularPolygon {
     // Polygon base class
     public:
+      class Invalid {}; // Used to throw exception
+
       RegularPolygon(const double w, const double h) { // w: width, h: height
+        // Base class constructor
+        // pre-conditions: Both width and height should be > 0
+
+        if(w < 1 || h < 1) { throw Invalid {}; } // check width/height validity
+
         width = w;
         height = h;
       }
--- pt-1-the-basics/chp-9-technicalities-classes-etc/examples/regular_polygon/src/main.cpp
+++ pt-1-the-basics/chp-9-technicalities-classes-etc/examples/regular_polygon/src/main.cpp
@@ -4,11 +4,19 @@
 #include "../headers/regular_polygon.h"
 using namespace Shape;
 
-int main() {
+int main() try {
   Triangle tri(7, 4);
   Square sqr(14, 14);
 
   std::cout << tri.type() << " area: " << tri.area() << '\n';
   std::cout << sqr.type() << " area: " << sqr.area() << '\n';
   return 0;
+}
+catch(RegularPolygon::Invalid) {
+  std::cerr << "Error: Both width and height must be greater than zero.\n";
+  return 1;
+}
+catch(...) {
+  std::cerr << "Error: Unknown exception.\n";
+  return 2;
 }
