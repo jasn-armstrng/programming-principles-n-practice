6d2bb61dcbd9996b2c21504349cc40a95dc370f3
Jason Armstrong <jasnarmstrng@gmail.com>
2023-01-29 22:33:12 -0400

    feat: Create Instrument class
    Add program description

--- pt-1-the-basics/chp-9-technicalities-classes-etc/examples/instrument/src/main.cpp
+++ pt-1-the-basics/chp-9-technicalities-classes-etc/examples/instrument/src/main.cpp
@@ -1,4 +1,14 @@
+// This program further practice with inheritance and virtual functions also
+// introduces pure virtual functions
+
 #include <iostream>
+
+class Instrument {
+  public:
+    void make_sound() {
+      std::cout << "Instrument playing ...\n";
+    }
+};
 
 int main() {
 
