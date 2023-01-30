748f38980d5de9b5af19bca40e8a2b453ffedd6a
Jason Armstrong <jasnarmstrng@gmail.com>
2023-01-30 08:51:17 -0400

    feat: Create astract class Smartphone
    Add pure virtual functions, take_selfie() and make_call()

--- pt-1-the-basics/chp-9-technicalities-classes-etc/examples/smartphone/src/main.cpp
+++ pt-1-the-basics/chp-9-technicalities-classes-etc/examples/smartphone/src/main.cpp
@@ -1,6 +1,12 @@
 // This program is used to understand abstraction
 #include <iostream>
 
+class Smartphone {
+  public:
+    virtual void take_selfie() = 0;
+    virtual void make_call() = 0;
+};
+
 int main() {
 
   return 0;
