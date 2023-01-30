7f8636c61c04168842614904c713579f84a4cd74
Jason Armstrong <jasnarmstrng@gmail.com>
2023-01-30 19:14:44 -0400

    refactor: Rename youtube_channel.cpp to main.cpp

--- pt-1-the-basics/chp-9-technicalities-classes-etc/examples/youtube_channel/headers/youtube_channel.h
+++ pt-1-the-basics/chp-9-technicalities-classes-etc/examples/youtube_channel/headers/youtube_channel.h
@@ -4,8 +4,6 @@
 
 class YouTubeChannel {
   public:
-  // interface
-
     // YouTubeChannel constructor
     YouTubeChannel(std::string name_, std::string owner_name_) {
       name = name_;
@@ -27,7 +25,7 @@ class YouTubeChannel {
     }
 
     void subscribe() {
-      // increment subscriber_count
+      // increments subscriber_count
       ++subscriber_count;
     }
 
@@ -41,35 +39,17 @@ class YouTubeChannel {
       published_video_titles.push_back(title);
     }
 
-    void like(){
-      ++likes;
-    }
+    void like() { ++likes; }
 
     // setters
-    void set_name(const std::string& new_name) {
-      name = new_name;
-    }
-
-    void set_owner(const std::string& new_owner) {
-      owner_name = new_owner;
-    }
-
-    void set_about(const std::string new_about) {
-      about = new_about;
-    }
+    void set_name(const std::string& new_name) { name = new_name; }
+    void set_owner(const std::string& new_owner) { owner_name = new_owner; }
+    void set_about(const std::string new_about) { about = new_about; }
 
     // getters
-    void get_name() {
-      std::cout << name << '\n';
-    }
-
-    void get_owner() {
-      std::cout << owner_name << '\n';
-    }
-
-    void get_about() {
-      std::cout << name << ": "<< about << '\n';
-    }
+    void get_name() { std::cout << name << '\n'; }
+    void get_owner() { std::cout << owner_name << '\n'; }
+    void get_about() { std::cout << name << ": "<< about << '\n'; }
 
     void check_analytics() {
       if(likes<5) {
@@ -80,7 +60,6 @@ class YouTubeChannel {
     }
 
   private:
-  // implementation details
     // encapsulation: These properties should be updated using a methods via
     // the class' public interface
     std::string name;
@@ -94,20 +73,16 @@ class YouTubeChannel {
     // members here are accessible to all derived classes
 };
 
-// here CookingYouTubeChannel (a derived class) inherits everything from
+// Here CookingYouTubeChannel (a derived class) inherits everything from
 // YouTubeChannel (the base class)
 class CookingYouTubeChannel:public YouTubeChannel {
-  public:                                                            // inherit the constructor from base
-    CookingYouTubeChannel(std::string name_, std::string owner_name_):YouTubeChannel(name_, owner_name_) {
-      // ...
-    }
+  public:
+    CookingYouTubeChannel(std::string name_, std::string owner_name_)
+      :YouTubeChannel(name_, owner_name_) {} // inherit the constructor from base
 };
 
-// here CookingYouTubeChannel (a derived class) inherits everything from
-// YouTubeChannel (the base class)
 class SingingYouTubeChannel:public YouTubeChannel {
-  public:                                                            // inherit the constructor from base
-    SingingYouTubeChannel(std::string name_, std::string owner_name_):YouTubeChannel(name_, owner_name_) {
-      // ...
-    }
+  public:
+    SingingYouTubeChannel(std::string name_, std::string owner_name_)
+      :YouTubeChannel(name_, owner_name_) {}
 };
