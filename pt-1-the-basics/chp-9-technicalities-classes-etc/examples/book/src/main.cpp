f9caeeb36b45592d3d00b197c632b7a51dcdb9ad
Jason Armstrong <jasnarmstrng@gmail.com>
2023-01-30 20:13:10 -0400

    feat: Create struct Book

--- pt-1-the-basics/chp-9-technicalities-classes-etc/examples/book/src/main.cpp
+++ pt-1-the-basics/chp-9-technicalities-classes-etc/examples/book/src/main.cpp
@@ -1,7 +1,20 @@
 // This program used to practice operator overloading
 #include <iostream>
 
-int main() {
+struct Book {
+  std::string name;
+  std::string author;
+  std::string genre;
 
+  // Constructor
+  Book(std::string n, std::string a, std::string g) {
+    name = n;
+    author = a;
+    genre = g;
+  }
+}; // namespace Book
+
+int main() {
+  Book b1 { "Far from the madding crowd", "Thomas Hardy", "English classic" };
   return 0;
 }
