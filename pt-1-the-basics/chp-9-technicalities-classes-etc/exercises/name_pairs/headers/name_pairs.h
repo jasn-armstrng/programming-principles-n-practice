95465cc966c57d0012c41f065dd25c98a704a0bc
Jason Armstrong <jasnarmstrng@gmail.com>
2023-02-01 12:48:53 -0400

    feat: Add member func read_names()

--- pt-1-the-basics/chp-9-technicalities-classes-etc/exercises/name_pairs/headers/name_pairs.h
+++ pt-1-the-basics/chp-9-technicalities-classes-etc/exercises/name_pairs/headers/name_pairs.h
@@ -3,6 +3,18 @@
 
 class Name_pairs {
 public:
+  void read_names() {
+    // reads a series of names from console input
+    std::string n,
+                msg { "Enter names below. To end enter a semi-colon (;):\n" };
+
+    std::cout << msg;
+
+    while(std::cin >> n) {
+      if(n == ";") { break; }
+      name.push_back(n);
+    }
+  }
 
 private:
   std::vector<std::string> name;
--- pt-1-the-basics/chp-9-technicalities-classes-etc/exercises/name_pairs/src/main.cpp
+++ pt-1-the-basics/chp-9-technicalities-classes-etc/exercises/name_pairs/src/main.cpp
@@ -3,6 +3,8 @@
 #include "../headers/name_pairs.h"
 
 int main() {
+  Name_pairs name_pairs;
+  name_pairs.read_names();
 
   return 0;
 }
