63400b1cf3b011364d23c6475b16f123630fce08
Jason Armstrong <jasnarmstrng@gmail.com>
2023-02-01 13:07:56 -0400

    feat: Add member func read_ages()

--- pt-1-the-basics/chp-9-technicalities-classes-etc/exercises/name_pairs/headers/name_pairs.h
+++ pt-1-the-basics/chp-9-technicalities-classes-etc/exercises/name_pairs/headers/name_pairs.h
@@ -4,15 +4,31 @@
 class Name_pairs {
 public:
   void read_names() {
-    // reads a series of names from console input
-    std::string n,
-                msg { "Enter names below. To end enter a semi-colon (;):\n" };
+    // reads a series of names from console input and stores them in name.
+    std::string n;
+    std::string msg { "Enter person names below. End by entering a semi-colon (;):\n" };
 
     std::cout << msg;
 
     while(std::cin >> n) {
       if(n == ";") { break; }
       name.push_back(n);
+    }
+
+    read_ages();
+  }
+
+  void read_ages() {
+    // reads the ages of the names entered using read_names(). Call inside of
+    // read_names
+    double a;
+    std::string msg { "Enter age of persons entered:\n" };
+
+    std::cout << msg;
+    for(unsigned i = 0; i < name.size(); ++i) {
+      std::cout << name[i] << ": ";
+      std::cin >> a;
+      age.push_back(a);
     }
   }
 
