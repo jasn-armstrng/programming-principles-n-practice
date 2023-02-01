2856229a32f84f05547ac5c6c240f64421e511ef
Jason Armstrong <jasnarmstrng@gmail.com>
2023-02-01 14:45:38 -0400

    docs: Update program description and comments

--- pt-1-the-basics/chp-9-technicalities-classes-etc/exercises/name_pairs/headers/name_pairs.h
+++ pt-1-the-basics/chp-9-technicalities-classes-etc/exercises/name_pairs/headers/name_pairs.h
@@ -1,40 +1,48 @@
 #include <iostream>
 #include <vector>
 
-class Person {
+/************************ Class/member definitions ****************************/
+class Person
+{
 public:
   std::string name;
   double age;
-
-  Person(std::string& n, double& a):name{n}, age{a} { }
+  // constructor
+  Person(std::string& n, double& a):name{n}, age{a} {}
 };
 
-class Name_pairs {
+class Name_pairs
+{
 public:
   std::vector<Person> person;
   void read_name_age();
 };
 
-void Name_pairs::read_name_age() {
+// read_name_age() definition
+void Name_pairs::read_name_age()
+{
   std::string msg { "Enter <Name Age> of persons. Enter <None 0> to end:\n" };
   std::string name;
   double age;
-
   // Prompt for name/age pairs and store
   std::cout << msg;
-  while(std::cin >> name >> age) {
+  while(std::cin >> name >> age)
+  {
     if (name == "None" && age == 0) { break; }
     Person p { name, age };
     person.push_back(p);
   }
-
   // Sort entries on name
   auto lambda = [](const Person &a, const Person &b) { return a.name < b.name; };
   std::sort(person.begin(), person.end(), lambda);
 }
 
-void operator<<(std::ostream& os, Name_pairs& np) {
-  for(Person p: np.person) {
+/*************************** Helper functions *********************************/
+// Overload << to print Name_pairs object
+void operator<<(std::ostream& os, Name_pairs& np)
+{
+  for(Person p: np.person)
+  {
     os << p.name << ", " << p.age << '\n';
   }
 }
--- pt-1-the-basics/chp-9-technicalities-classes-etc/exercises/name_pairs/src/main.cpp
+++ pt-1-the-basics/chp-9-technicalities-classes-etc/exercises/name_pairs/src/main.cpp
@@ -1,8 +1,14 @@
-// Program description:
+/*******************************************************************************
+Description: Reads and stores <Name Age> pairs from user input. Prints pairs to
+             console.
+Created:     Feb 1, 2023
+To do:       Handle bad <Name Age> inputs
+*******************************************************************************/
 #include <iostream>
 #include "../headers/name_pairs.h"
 
-int main() {
+int main()
+{
   Name_pairs name_pairs;
   name_pairs.read_name_age();
   std::cout << name_pairs;
