8560068126fcf6d3040fde9ffdbe2c5fda81d488
Jason Armstrong <jasnarmstrng@gmail.com>
2023-02-01 14:34:22 -0400

    refactor: Move read_name_age() def out class

--- pt-1-the-basics/chp-9-technicalities-classes-etc/exercises/name_pairs/headers/name_pairs.h
+++ pt-1-the-basics/chp-9-technicalities-classes-etc/exercises/name_pairs/headers/name_pairs.h
@@ -12,25 +12,26 @@ public:
 class Name_pairs {
 public:
   std::vector<Person> person;
-
-  void read_name_age() {
-    std::string msg { "Enter <Name Age> of persons. Enter <None 0> to end:\n" };
-    std::string name;
-    double age;
-
-    // Prompt for name/age pairs and store
-    std::cout << msg;
-    while(std::cin >> name >> age) {
-      if (name == "None" && age == 0) { break; }
-      Person p { name, age };
-      person.push_back(p);
-    }
-
-    // Sort entries on name
-    auto lambda = [](const Person &a, const Person &b) { return a.name < b.name; };
-    std::sort(person.begin(), person.end(), lambda);
-  }
+  void read_name_age();
 };
+
+void Name_pairs::read_name_age() {
+  std::string msg { "Enter <Name Age> of persons. Enter <None 0> to end:\n" };
+  std::string name;
+  double age;
+
+  // Prompt for name/age pairs and store
+  std::cout << msg;
+  while(std::cin >> name >> age) {
+    if (name == "None" && age == 0) { break; }
+    Person p { name, age };
+    person.push_back(p);
+  }
+
+  // Sort entries on name
+  auto lambda = [](const Person &a, const Person &b) { return a.name < b.name; };
+  std::sort(person.begin(), person.end(), lambda);
+}
 
 void operator<<(std::ostream& os, Name_pairs& np) {
   for(Person p: np.person) {
