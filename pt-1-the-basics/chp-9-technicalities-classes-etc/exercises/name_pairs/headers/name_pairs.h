7e1a8c19e656c77e8de29b5b40d5870f64048c70
Jason Armstrong <jasnarmstrng@gmail.com>
2023-02-01 13:34:44 -0400

    feat: Create Person class

--- pt-1-the-basics/chp-9-technicalities-classes-etc/exercises/name_pairs/headers/name_pairs.h
+++ pt-1-the-basics/chp-9-technicalities-classes-etc/exercises/name_pairs/headers/name_pairs.h
@@ -1,10 +1,18 @@
 #include <iostream>
 #include <vector>
 
+class Person {
+public:
+  std::string name;
+  double age;
+
+  Person(std::string& n, double& a):name{n}, age{a} { }
+};
+
 class Name_pairs {
 public:
   void read_names() {
-    // reads a series of names from console input and stores them in name.
+    // reads a series of names from console input and stores them in name
     std::string n;
     std::string msg { "Enter person names below. End by entering a semi-colon (;):\n" };
 
@@ -14,21 +22,28 @@ public:
       if(n == ";") { break; }
       name.push_back(n);
     }
-
-    read_ages();
   }
 
   void read_ages() {
-    // reads the ages of the names entered using read_names(). Call inside of
-    // read_names
+    // reads the ages of the names entered using read_names()
     double a;
     std::string msg { "Enter age of persons entered:\n" };
 
     std::cout << msg;
+
     for(unsigned i = 0; i < name.size(); ++i) {
       std::cout << name[i] << ": ";
       std::cin >> a;
       age.push_back(a);
+    }
+  }
+
+  void print() {
+    // prints each name/age pair stored (one per line)
+    std::cout << "\nName, Age"
+              << "\n=========\n";
+    for(unsigned i = 0; i < name.size(); ++i) {
+      std::cout << name[i] << ", " << age[i] << '\n';
     }
   }
 
--- pt-1-the-basics/chp-9-technicalities-classes-etc/exercises/name_pairs/src/main.cpp
+++ pt-1-the-basics/chp-9-technicalities-classes-etc/exercises/name_pairs/src/main.cpp
@@ -5,6 +5,8 @@
 int main() {
   Name_pairs name_pairs;
   name_pairs.read_names();
+  name_pairs.read_ages();
+  name_pairs.print();
 
   return 0;
 }
