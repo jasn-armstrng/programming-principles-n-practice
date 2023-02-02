950f26fc69cfebedcce8fc8fea609d151b741a7a
Jason Armstrong <jasnarmstrng@gmail.com>
2023-02-01 22:36:40 -0400

    feat: Add simple ISBN number validation
    Throw Invalid {} when bad ISBN number entered
    Handle error thrown in main()

--- pt-1-the-basics/chp-9-technicalities-classes-etc/exercises/library/headers/library.h
+++ pt-1-the-basics/chp-9-technicalities-classes-etc/exercises/library/headers/library.h
@@ -11,13 +11,33 @@
 
 #include <iostream>
 
+class Invalid {};
+bool is_isbn(std::string& ISBN);
+
 class Book
 {
 public:
   Book(std::string ISBN, std::string Title, std::string Author, std::string Copyright_Date)
-    :isbn{ISBN}, title{Title}, author{Author}, copyright_date{Copyright_Date} {};
+    :isbn{ISBN}, title{Title}, author{Author}, copyright_date{Copyright_Date}
+  {
+    if(!is_isbn(ISBN)) { throw Invalid {}; }
+  };
 
 private:
   std::string isbn, title, author, copyright_date;
   bool checked_out;
 };
+
+bool is_isbn(std::string& ISBN)
+{
+  // a valid ISBN-13,
+  // - starts with 978
+  // - is 13 digits long
+  if(ISBN.size() != 14) { return false; }
+  if(ISBN.substr(0, 4) != "978-") { return false; }
+  for(unsigned i = 4; i < ISBN.size(); ++i)
+  {
+    if(!std::isdigit(ISBN[i])) { return false; }
+  }
+  return true;
+}
--- pt-1-the-basics/chp-9-technicalities-classes-etc/exercises/library/src/main.cpp
+++ pt-1-the-basics/chp-9-technicalities-classes-etc/exercises/library/src/main.cpp
@@ -12,8 +12,20 @@
 #include <iostream>
 #include "../headers/library.h"
 
-int main()
+int main() try
 {
-  Book the_hobbit { "0618968636", "The Hobbit", "J. R. R. Tolkien", "January 1, 1973" };
+  Book the_hobbit { "978-0618968636&", "The Hobbit", "J. R. R. Tolkien", "January 1, 1973" };
+  // std::string isbn { "978-0618968636" };
+  // std::cout << is_isbn(isbn) << '\n';
   return 0;
+}
+catch(Invalid)
+{
+  std::cerr << "Error: ISBN should be in the form 978-XXXXXXXXXX, where X is an integer.\n";
+  return 1;
+}
+catch(...)
+{
+  std::cerr << "Error: Unknown exception!\n";
+  return 2;
 }
