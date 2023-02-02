6bd59859759cd7cb351947c14c7cb37a37a775d1
Jason Armstrong <jasnarmstrng@gmail.com>
2023-02-01 20:18:00 -0400

    Create new project library
    This project implements the solutions for Chp 9's exercise 5-9 of text.

---
+++ pt-1-the-basics/chp-9-technicalities-classes-etc/exercises/library/headers/library.h
@@ -0,0 +1,10 @@
+/**
+ * @file library.h
+ * @author Jason Armstrong
+ * @brief Contains the definitions for classes and helper functions
+ * @version 0.1
+ * @date 2023-02-01
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
---
+++ pt-1-the-basics/chp-9-technicalities-classes-etc/exercises/library/src/main.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file main.cpp
+ * @author Jason Armstrong
+ * @brief
+ * @version 0.1
+ * @date 2023-02-01
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+#include <iostream>
+#include "../headers/library.h"
+
+int main()
+{
+  return 0;
+}
