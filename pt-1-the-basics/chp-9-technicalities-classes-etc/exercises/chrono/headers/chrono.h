8c1e52d6def7fc77a50badff68bfcc293a250fc1
Jason Armstrong <jasnarmstrng@gmail.com>
2023-02-10 12:00:54 -0400

    feat: Create Date and supporting Month classes

--- pt-1-the-basics/chp-9-technicalities-classes-etc/exercises/chrono/headers/chrono.h
+++ pt-1-the-basics/chp-9-technicalities-classes-etc/exercises/chrono/headers/chrono.h
@@ -5,3 +5,21 @@
  * @version 0.1
  * @date 2023-02-10
  */
+
+namespace Chrono
+{
+  enum class Month // Used to construct a new date in the form dd/mm/yyyy
+  {
+    jan=1, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec
+  };
+
+  class Date
+  {
+    Date(int& day, Month& month, int& year):dd{day}, mm{month}, yyyy{year} { }
+
+    private:
+      int dd;
+      Month mm;
+      int yyyy;
+  };
+};
